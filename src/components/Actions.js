43c90314bdc7288d8d0ff9e8dc11387ea739a073
Sherab Pereira <sherabpereira@gmail.com>
2021-09-20 17:01:46 +0200

    Lab done

---
+++ src/components/Actions.js
@@ -0,0 +1,21 @@
+function Actions(props) {
+  return (
+    <div className="actions">
+      {/* Font Awesome icons */}
+      <a href="#">
+        <i className="far fa-comment"></i>{' '}
+      </a>
+      <a href="#">
+        <i className="fas fa-retweet"></i>{' '}
+      </a>
+      <a href="#">
+        <i className="far fa-heart"></i>{' '}
+      </a>
+      <a href="#">
+        <i className="fas fa-share"></i>
+      </a>
+    </div>
+  )
+}
+
+export default Actions
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,39 +1,32 @@
-function Tweet() {
+import Actions from './Actions.js'
+import Message from './Message.js'
+import ProfileImage from './ProfileImage.js'
+import Timestamp from './Timestamp.js'
+import User from './User.js'
+
+function Tweet(props) {
   return (
     <div className="tweet">
-      <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
-        className="profile"
-        alt="profile"
-      />
+      <ProfileImage imgSrc={props.tweet.user.image} />
 
       <div className="body">
+
         <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
-
-          <span className="timestamp">Nov 30, 2020</span>
+          <User
+            user={props.tweet.user}
+          />
+          <Timestamp timestamp={props.tweet.timestamp} />
         </div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i class="far fa-comment"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="far fa-heart"></i>
-          <i class="fas fa-share"></i>
-        </div>
+        <Message message={props.tweet.message} />
+        <Actions />
       </div>
 
-      <i class="fas fa-ellipsis-h"></i>
+      <a href="#" className="more">
+        <i className="fas fa-ellipsis-h"></i>
+      </a>
     </div>
-  );
+  )
 }
 
-export default Tweet;
+export default Tweet
