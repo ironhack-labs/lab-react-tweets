8ed5e5e911ec4111b2a79ba7db94bf39c2eecae7
Guille Celma <guillecelmaan@hotmail.es>
2021-09-20 16:34:25 +0200

    done

---
+++ src/components/Message.js
@@ -0,0 +1,9 @@
+const Message = props =>{
+    return(
+        <p className="message">
+          {props.message}
+        </p>
+    )
+}
+
+export default Message
---
+++ src/components/User.js
@@ -0,0 +1,14 @@
+
+
+
+const User =(props)=>{
+   
+    return(
+        <span className="user">
+                <span className="name">{props.userData.name}</span>
+                <span className="handle">{props.userData.handle}</span>
+        </span>
+    )
+}
+
+export default User
