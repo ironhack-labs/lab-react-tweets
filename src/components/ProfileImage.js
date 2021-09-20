b28de71442a360c415afa70375613c8808befcb7
lauraAlabau <arlaura3@gmail.com>
2021-09-20 16:58:08 +0200

    Iteration 4 | ProfileImage Component

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,11 @@
+function ProfileImage(props) {
+    return (
+        <div className="tweet">
+            <img src={props.image} className="profile" alt="profile"/>        
+        </div>
+  
+    );
+  }
+  
+  export default ProfileImage;
+  
