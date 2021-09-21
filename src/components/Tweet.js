1e5cb416e9aecd69cb6e3c221eb02bf756f6b984
Jaume Ballester <86907672+jaumebt90@users.noreply.github.com>
2021-09-21 17:09:39 +0200

    Update Tweet.js

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -5,7 +5,6 @@ import Message from "./Message";
 import Actions from "./Actions";
 
 function Tweet(props) {
-  console.log("PROPIEDADES DEL TWEET", props);
   return (
     <div className="tweet">
       <ProfileImage profileImage={props.tweet.user.image} />
