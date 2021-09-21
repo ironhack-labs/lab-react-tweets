914f11b5f753cbf589a566b0a1384f01d923fe66
Ninabaldo <nbaldorousseau@gmail.com>
2021-09-21 15:46:51 +0200

    not donne at all

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import Tweet from "./components/Tweet";
-
 const tweetsArray = [
   {
     user: {
@@ -35,11 +34,16 @@ const tweetsArray = [
 ];
 
 function App() {
+
   return (
     <div className="App">
       <Tweet />
+    <div className='App'>
+      <Tweet tweet={tweetsArray[0]} />
+      <Tweet tweet={tweetsArray[1]} />
+      <Tweet tweet={tweetsArray[2]} />
+    </div>
     </div>
   );
 }
-
 export default App;
