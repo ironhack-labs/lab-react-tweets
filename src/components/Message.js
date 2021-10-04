1df90d736ed9d0f7e21690138b76ec8590d4e285
faxvargas <fab.med1011@gmail.com>
2021-10-04 18:59:24 -0500

    done

---
+++ src/components/Message.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Message(props) {
+    return (
+        <div>
+            <p className="message">
+         {props.message}
+        </p>
+        </div>
+    )
+}
+
+export default Message
