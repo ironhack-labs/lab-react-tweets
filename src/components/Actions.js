744147309ad71d8933316d87b9ae7e2f71e2083c
Telma Rosário <telmarrosario@gmail.com>
2021-11-29 17:54:21 +0000

    done

---
+++ src/components/Actions.js
@@ -0,0 +1,15 @@
+function Actions () {
+    return(
+        <div>
+                    <div className="actions">
+          {/* Font Awesome icons */}
+          <i class="far fa-comment"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="far fa-heart"></i>
+          <i class="fas fa-share"></i>
+        </div>
+        </div>
+    )
+};
+
+export default Actions;
---
+++ src/components/Timestamp.js
@@ -0,0 +1,10 @@
+function Timestamp (props) {
+    return(
+        <div>
+          <span className="timestamp">{props.time}</span>
+
+        </div>
+    )
+};
+
+export default Timestamp;
