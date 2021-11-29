63324a37af92b4fa4655e99a14c4cda154649949
JosePedro <guedes.jose@gmail.com>
2021-11-29 18:08:46 +0000

    finishedlab

---
+++ src/components/Actions.js
@@ -0,0 +1,13 @@
+function Actions() {
+  return (
+    <div className="actions">
+      {/* Font Awesome icons */}
+      <i class="far fa-comment"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="far fa-heart"></i>
+      <i class="fas fa-share"></i>
+    </div>
+  );
+}
+
+export default Actions;
---
+++ src/components/Message.js
@@ -0,0 +1,10 @@
+function Message(props) {
+  return (
+    <p className="message">
+      {props.message}
+      🚀
+    </p>
+  );
+}
+
+export default Message;
