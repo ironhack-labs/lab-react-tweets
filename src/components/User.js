0fd63e3929cb0471f0a5b141b63823b388cf065f
Jose Gutierrez <josgutcej@gmail.com>
2021-11-29 20:25:57 +0100

    finito pero con dudas

---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+const User = (props) => {
+    const { user } = props
+    return (
+        <span className="user">
+            <span className="name">{user.name}</span>
+            <span className="handle">@{user.handle}</span>
+        </span>
+    )
+}
+
+export default User
