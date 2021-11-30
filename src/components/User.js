5d4d138120d973513de5163d951ed2173bfd487f
Anne Dantas <annedantas@gmail.com>
2021-11-30 09:49:02 +0000

    just adding a @ before the handle name

--- src/components/User.js
+++ src/components/User.js
@@ -2,7 +2,7 @@ const User = (props) => {
   return (
     <span className="user">
       <span className="name">{props.name}</span>
-      <span className="handle">{props.handle}</span>
+      <span className="handle">@{props.handle}</span>
     </span>
   );
 };
