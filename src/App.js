d588016f844de973c92380750d28a326c198300b
Amy Davies <amyloudav@gmail.com>
2021-11-30 09:23:47 +0100

    updated with iteration 9

--- src/App.js
+++ src/App.js
@@ -37,7 +37,9 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet tweet={ tweetsArray[0]}/>
+      {tweetsArray.map((el) => {
+        return <Tweet tweet={el} />;
+      })}
     </div>
   );
 }
