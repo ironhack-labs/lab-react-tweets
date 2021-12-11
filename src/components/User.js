96b8e338da267033d4d9f463841bec780b89970f
rodrigosamaniego <rodrigosamaniegopiza@gmail.com>
2021-12-10 18:51:43 -0600

    Todas las iteraciones listas uwu

---
+++ src/components/User.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const User = ({ user }) => {
+    return (
+        <div>
+            <span className="user">
+                <span className="name"> {user.name} </span>
+                <span className="handle">{user.handle}</span>
+            </span>
+        </div>
+    )
+}
+
+export default User
