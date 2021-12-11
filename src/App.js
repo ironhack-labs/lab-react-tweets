0b8992728144df8604965c91f70d47f37250ad88
Hector Biminchumo <hectorbiminchumo@gmail.com>
2021-12-10 19:22:32 -0500

    lab-react-tweets done

--- src/App.js
+++ src/App.js
@@ -37,7 +37,9 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet tweet={ tweetsArray[0]} />
+      { tweetsArray.map((tuit) =>{
+                  return <Tweet tweet={tuit} /> 
+      })} 
     </div>
   );
 }
