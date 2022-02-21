953dbab8c39d45459e37ceacda6cf3632ec269c3
Mario Diaz <mariodzls@gmail.com>
2022-02-21 16:36:30 +0100

    Hecho

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,11 @@
+const ProfileImage = (props) => {
+    return (<div>
+        <img
+    src={props.image}
+    className="profile"
+    alt="profile"
+  /> </div>)
+}
+
+
+export default ProfileImage
---
+++ src/components/User.js
@@ -0,0 +1,8 @@
+const User = (props) => {
+   return( <span className="user">
+            <span className="name">{props.name}</span>
+            <span className="handle">@{props.handle}</span>
+          </span>)
+}
+
+export default User
