26b690cb36180af71cace1fd9ac3f30d9a83021c
Clara Villa <Clara.Villa@lego.com>
2022-02-21 14:40:22 +0000

    add i tag back in

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -11,13 +11,13 @@ function Tweet(props) {
       <div className="body">
         <div className="top">
           <User userData={props.tweet.user} />
-
           <Timestamp time={props.tweet.timestamp} />
         </div>
 
         <Message message={props.tweet.message} />
         <Actions />
       </div>
+      <i class="fas fa-ellipsis-h"></i>
     </div>
   );
 }
