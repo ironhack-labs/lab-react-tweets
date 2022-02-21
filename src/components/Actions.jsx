36bf49cb7f7feb7dda7287d8e5ccb3d31df1e486
Lamer217 <males.public@gmail.com>
2022-02-21 16:40:59 +0100

    do iteration 8

---
+++ src/components/Actions.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Actions() {
+  return (
+    <div className="actions">
+      <i class="far fa-comment"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="far fa-heart"></i>
+      <i class="fas fa-share"></i>
+    </div>
+  );
+}
