e471efa142893d53b0426792830bfe18996b6e97
NielsMKristensen <Niels.Kristensen@Hotmail.com>
2022-02-21 15:30:44 +0100

    Done

--- src/App.js
+++ src/App.js
@@ -37,7 +37,11 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet />
+      <Tweet tweet={ tweetsArray[0]} />
+      <hr></hr>
+      <Tweet tweet={ tweetsArray[1]} />
+      <hr></hr>
+      <Tweet tweet={ tweetsArray[2]} />
     </div>
   );
 }
---
+++ src/components/Message.js
@@ -0,0 +1,10 @@
+function Message(props) {
+    console.log(props)
+    return (
+          <p className="message">
+            {props.message}
+          </p>
+    );
+  }
+  
+  export default Message;
