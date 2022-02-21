fd0fb379cbb79ebb73d116824906e7e4794da225
Lamer217 <males.public@gmail.com>
2022-02-21 16:39:25 +0100

    do iteartion 7

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,6 +1,7 @@
 import ProfileImage from './ProfileImage';
 import User from './User';
 import Timestamp from './Timestamp';
+import Message from './Message';
 
 function Tweet(props) {
   const { message, timestamp, user } = props.tweet;
@@ -14,7 +15,7 @@ function Tweet(props) {
           <Timestamp timestamp={timestamp} />
         </div>
 
-        <p className="message">{message}</p>
+        <Message message={message} />
 
         <div className="actions">
           {/* Font Awesome icons */}
