a53a1a82daad92757c13607ecf2bb8ae76e79cb5
Bart Huiskamp <bartolameus.h@gmail.com>
2022-02-21 21:28:20 +0100

    bart finished tweet Lab

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,6 @@
+function profileProfileImage(props){
+    return  <img src={props.image} className="profile" alt="profile"/>
+    
+}
+
+export default profileProfileImage;
---
+++ src/components/Timestamp.js
@@ -0,0 +1,8 @@
+function Timestamp(props) {
+    return(
+    
+        <span className="timestamp"> {props.time} </span>
+        )
+    }
+    
+    export default Timestamp;
