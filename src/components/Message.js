dc2fa7b56f36f4800ef7e150826e97bfc0fb9d8c
Leonardo <mr_kunkel@hotmail.com>
2022-02-22 10:14:19 -0600

    Iteration 7 & 8

---
+++ src/components/Message.js
@@ -0,0 +1,6 @@
+
+export default function Message() {
+  return (
+    <p className="message"> the human likes to say. that i live here rent free. but i would argue. this housing accommodation. is my payment. for a lifetime of love. and excellent company </p>
+  )
+}
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,3 +1,5 @@
+import Actions from "./Actions";
+import Message from "./Message";
 import ProfileImage from "./ProfileImage";
 import Timestamp from "./Timestamp";
 import User from "./User";
@@ -14,21 +16,11 @@ function Tweet(props) {
 
           <User userData={props.tweet.user} />
           <Timestamp time={props.tweet.timestamp} />
-
-          <span className="timestamp">{ props.tweet.timestamp }</span>
         </div>
 
-        <p className="message">
-        { props.tweet.message }
-        </p>
+        <Message message={props.tweet.message} />
 
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i class="far fa-comment"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="far fa-heart"></i>
-          <i class="fas fa-share"></i>
-        </div>
+        <Actions />
       </div>
 
       <i class="fas fa-ellipsis-h"></i>
