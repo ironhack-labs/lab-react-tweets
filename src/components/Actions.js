173bf44bc567b2d0a6bb70833ab3a025b1ac4d0f
Alan Cruz <alanchis@hotmail.com>
2022-02-21 18:28:08 -0600

    lab 21feb done

---
+++ src/components/Actions.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Actions() {
+  return (
+    <div>
+        <div className="actions">
+        <i class="far fa-comment"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="far fa-heart"></i>
+        <i class="fas fa-share"></i>
+    </div>
+    
+    </div>
+  )
+}
