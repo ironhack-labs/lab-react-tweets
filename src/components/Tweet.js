a1ad0c66001c39af21e128df92f592bf592d6906
Hollie Duncan <holduncan7@gmail.com>
2022-02-28 17:02:29 +0000

    Completed and Styled

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,34 +1,30 @@
-function Tweet() {
+// Imports
+
+import ProfileImage from "./ProfileImage";
+import User from "./User";
+import Timestamp from "./Timestamp";
+import Message from "./Message";
+import Action from "./Action";
+
+// Function
+
+function Tweet(props) {
   return (
     <div className="tweet">
-      <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
-        className="profile"
-        alt="profile"
-      />
+      <ProfileImage image={props.tweet.user.image}/>
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
 
-          <span className="timestamp">Nov 30, 2020</span>
+          <User userdata={props.tweet.user}/>
+          <Timestamp time={props.tweet.timestamp} />
+          
         </div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
+        <Message message={props.tweet.message}/>
 
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i class="far fa-comment"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="far fa-heart"></i>
-          <i class="fas fa-share"></i>
-        </div>
+        <Action />
+        
       </div>
 
       <i class="fas fa-ellipsis-h"></i>
