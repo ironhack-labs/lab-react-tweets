2eb98fe4d7610d40cedaa060a99db14e787406e1
Corentin Aesch <corentin.aesch@gmail.com>
2022-03-12 12:00:53 +0000

    Map adedd

--- src/App.js
+++ src/App.js
@@ -37,9 +37,9 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet tweet={ tweetsArray[0] }/>
-      <Tweet tweet={ tweetsArray[1] }/>
-      <Tweet tweet={ tweetsArray[2] }/>
+      {tweetsArray.map((twit) => {
+      return <Tweet tweet={twit}/>
+      })}
     </div>
   );
 }
