b3aeb472032e0ded62d04bf5d659369ddd22cbea
María Fdez de la Rosa <7mariadlrosa@gmail.com>
2022-05-02 15:34:45 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Tweet from "./components/Tweet";
 
+
 const tweetsArray = [
   {
     user: {
@@ -37,9 +38,12 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet />
+      <Tweet tweet={tweetsArray[0]} />
+      <Tweet tweet={tweetsArray[1]} />
+      <Tweet tweet={tweetsArray[2]} />
     </div>
   );
 }
 
-export default App;
+
+export default App; 
