435d9bf2f62f0d7619bb786278e15a4b9d6a4213
Jawid <41361157+infjay@users.noreply.github.com>
2022-05-09 21:37:50 +0200

    done

---
+++ src/components/Actions.js
@@ -0,0 +1,11 @@
+function Actions(props){
+    return (    
+    <div className="actions">
+    <i className="far fa-comment"></i>
+    <i className="fas fa-share"></i>
+    <i className="fas fa-retweet"></i>
+    <i className="far fa-heart"></i>
+  </div>);
+}
+
+export default Actions;
