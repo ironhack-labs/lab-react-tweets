37fe5530e68335ebff372b2a657372302753891d
jidekickpush <jeandenisdrane@gmail.com>
2022-05-09 23:48:27 +0200

    All iteration done

---
+++ src/components/User.js
@@ -0,0 +1,8 @@
+const User = (props) => {
+    return <span className="user">
+        <span className="name">{props.userData.name}</span>
+        <span className="handle">@{props.userData.handle}</span>
+    </span>
+}
+
+export default User
