5dad8fb09d5760caf57d259df9914b2377824c50
Thibaut Lartigue <lartiguethibaut@gmail.com>
2022-05-09 20:40:27 +0200

    Iteration 8 done

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -2,6 +2,8 @@ import ProfileImage from "./ProfileImage";
 import User from "./User";
 import Timestamp from "./Timestamp";
 import Message from "./Message";
+import Actions from "./Actions";
+
 
 
 function Tweet(props) {
@@ -17,13 +19,7 @@ function Tweet(props) {
 
         <Message message={props.tweet.message} />
 
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i class="far fa-comment"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="far fa-heart"></i>
-          <i class="fas fa-share"></i>
-        </div>
+        <Actions />
       </div>
 
       <i class="fas fa-ellipsis-h"></i>
