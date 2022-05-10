424d5afe0835bec1a3fd6b4828a2f2eb88c3cfc9
David Garcia <metaphysicaless1489@gmail.com>
2022-05-09 23:26:04 -0600

    lab finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Tweet from "./components/Tweet";
 
+
 const tweetsArray = [
   {
     user: {
@@ -34,10 +35,16 @@ const tweetsArray = [
   },
 ];
 
-function App() {
+function App(props) {
+  
+
+
+  
   return (
     <div className="App">
-      <Tweet />
+      <Tweet tweet={ tweetsArray[0].image} />
+      <Tweet tweet={ tweetsArray[1].image} />
+      <Tweet tweet={ tweetsArray[2].image} />
     </div>
   );
 }
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,37 +1,29 @@
-function Tweet() {
+import ProfilePic from "./ProfileImage";
+import User from "./User";
+import Timestamp from "./Timestamp";
+import Message from "./Message"
+import Actions from "./Actions"
+
+function Tweet(props) {
   return (
     <div className="tweet">
-      <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
-        className="profile"
-        alt="profile"
-      />
+      <ProfilePic Image = {props.tweet.user.image}/>
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
 
-          <span className="timestamp">Nov 30, 2020</span>
+          <User userdata={props.tweet.username.image}/>
+          <Timestamp time={props.tweet.timestamp}/>
+        </div>
+          
+
+          <Message message={props.tweet.message}/>
+          
+
+        <Actions/>
         </div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i class="far fa-comment"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="far fa-heart"></i>
-          <i class="fas fa-share"></i>
-        </div>
-      </div>
-
-      <i class="fas fa-ellipsis-h"></i>
+        <i class="fas fa-ellipsis-h"></i>
     </div>
   );
 }
