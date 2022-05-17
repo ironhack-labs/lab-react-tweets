da195f90ee490ae81d52463058b115ba8a292380
Alfonso Cano <alfonsoheartagram@gmail.com>
2022-05-17 13:58:57 -0500

    done

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const tweetsArray = [
     },
     timestamp: "2h ago",
     message:
-      "sometimes. the human presses their noggin against mine. to figure out what i’m thinking. so i just think really hard. about how much i love them. and hope they figure it out",
+      "sometimes. the human presses their noggin against mine. to figure out what i am thinking. so i just think really hard. about how much i love them. and hope they figure it out",
   },
   {
     user: {
@@ -37,7 +37,9 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet />
+      <Tweet tweet={ tweetsArray[0]} />
+      <Tweet tweet={ tweetsArray[1]} />
+      <Tweet tweet={ tweetsArray[2]} />
     </div>
   );
 }
