00db58f4e8614cfa172b89014ff5be6095694556
joaoGon <jpgon@live.com.pt>
2022-05-23 15:46:01 +0100

    lab done

--- src/App.js
+++ src/App.js
@@ -37,7 +37,9 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet />
+     <Tweet tweet={ tweetsArray[0] }/>
+     <Tweet tweet={ tweetsArray[1] }/>
+     <Tweet tweet={ tweetsArray[2] }/>
     </div>
   );
 }
---
+++ src/components/Message.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Message(props) {
+  return (
+    <p className="message">{props.message}</p>
+  )
+}
+
+export default Message
