bdcfc90f2e61ab0b457bff9c1b3a5647dbd3896f
Mariana Pontes <marianasevinatep@gmail.com>
2022-05-23 16:19:54 +0100

    done

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,34 +1,26 @@
-function Tweet() {
+import React from "react";
+import ProfileImage from "./ProfileImage";
+import Timestamp from "./Timestamp";
+import User from "./User";
+import Message from "./Message";
+import Actions from "./Actions";
+
+function Tweet(props) {
+  const { user, timestamp, message } = props.tweet;
   return (
     <div className="tweet">
-      <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
-        className="profile"
-        alt="profile"
-      />
+      <ProfileImage image={user.image} />
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
+          <User user={user} />
 
-          <span className="timestamp">Nov 30, 2020</span>
+          <Timestamp timestamp={timestamp} />
         </div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
+        <Message message={message} />
 
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i class="far fa-comment"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="far fa-heart"></i>
-          <i class="fas fa-share"></i>
-        </div>
+        <Actions />
       </div>
 
       <i class="fas fa-ellipsis-h"></i>
