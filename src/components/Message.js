ffdcdbd955bfcab8be36639259945a9b712bf38b
João Antunes <joaocbantunes@gmail.com>
2022-05-23 15:58:41 +0100

    lab done

---
+++ src/components/Message.js
@@ -0,0 +1,11 @@
+function Message(props) {
+  return (
+    <div>
+      <p>
+        <p className="message">{props.message} </p>
+      </p>
+    </div>
+  );
+}
+
+export default Message;
---
+++ src/components/Timestamp.js
@@ -0,0 +1,9 @@
+function Timestamp(props) {
+  return (
+    <div>
+      <span className="timestamp"> {props.time} </span>
+    </div>
+  );
+}
+
+export default Timestamp;
