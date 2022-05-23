1ce775b590397525ae416a0ab0fcd6597345d9c6
pedrocncosta <pedrocncosta@gmail.com>
2022-05-23 16:22:54 +0100

    Done

---
+++ src/components/ProfileImage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ProfileImage(props) {
+    
+  return (
+    <div>
+      <img src={props.img} className="profile" alt="profile" />
+    </div>
+  );
+}
+
+export default ProfileImage
