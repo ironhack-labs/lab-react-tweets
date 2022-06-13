dfe7ff6858862e25b7cbd2f6fffe1459b95fcd8c
robciu22 <rolegs@etik.com>
2022-06-13 17:07:37 +0200

    labReactTweetsDone

---
+++ src/components/Timestamp.js
@@ -0,0 +1,12 @@
+function Timestamp(tweet) {
+
+
+    return ( <
+        div >
+        <
+        span className = "timestamp" > { tweet.timestamp } < /span> <
+        /div>
+    );
+}
+
+export default Timestamp;
