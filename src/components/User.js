07caa51c5cf51f91d34c7f50fb2e301da9b97ee5
caetanolmsouza <caetanomartins@hotmail.com>
2022-06-20 17:05:22 +0200

    finished lab

---
+++ src/components/User.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function User(props) {
+  console.log("props from user are", props);
+  return (
+    <span className="user">
+      <span className="name">{props.userData.name}</span>
+      <span className="handle">{props.userData.handle}</span>
+    </span>
+  );
+}
+
+export default User;
