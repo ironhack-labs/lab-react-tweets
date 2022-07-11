ded669074a87d024dcf5e1f2399ed875cebaebbe
Jorge <jhermotorrado@gmail.com>
2022-07-11 16:22:43 +0200

    [MAD DE 0522]JorgeHermo Santiago Moncada

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,34 +1,29 @@
-function Tweet() {
+import ProfileImage from './ProfileImage'
+import User from './User'
+import Timestamp from './Timestamp'
+import Message from './Message'
+import Actions from './Actions'
+
+function Tweet({ tweet }) {
+  console.log(tweet)
+  const { user, timestamp, message } = tweet
+
+  const { name, image, handle } = user
+
   return (
     <div className="tweet">
-      <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
-        className="profile"
-        alt="profile"
-      />
+      <ProfileImage image={image} />
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
+          <User user={user} />
 
-          <span className="timestamp">Nov 30, 2020</span>
+          <Timestamp time={timestamp} />
+
         </div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i class="far fa-comment"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="far fa-heart"></i>
-          <i class="fas fa-share"></i>
-        </div>
+        <Message message={message} />
+        <Actions />
       </div>
 
       <i class="fas fa-ellipsis-h"></i>
