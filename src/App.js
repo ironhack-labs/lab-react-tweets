44e25aedef87c2dfb2f2421cb418842944287ed1
devgirl1992 <azadeh.galidari.1992@gmail.com>
2022-07-11 15:26:27 +0200

    lab-react-tweets done

--- src/App.js
+++ src/App.js
@@ -37,9 +37,61 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet />
+      <Tweet tweet={ tweetsArray[0] }/>
+      <Tweet tweet={ tweetsArray[1] }/>
+      <Tweet tweet={ tweetsArray[2] }/>
+      
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+// function App() {
+
+//   const user = {
+//     name: 'Rick Sanchez',
+//     email: 'rick@c-137.com',
+//     picture: 'https://images.unsplash.com/photo-1517849845537-4d257902454a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+//   }
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <Profile data={user} />
+//       </header>
+//     </div>
+//   )
+// }
+
+
+
+
+// function Profile(props) {
+// 	console.log(props)
+// 	return (
+// 		<>
+// 			<Details name={props.data.name} email={props.data.email} />
+// 			<Picture image={props.data.picture} />
+// 		</>
+// 	)
+// }
+
+
+// function Details(props) {
+// 	const email = props.email
+// 	const name = props.name
+// 	return (
+// 		<>
+// 			<h1>Name: {name}</h1>
+// 			<h1>Eamil: {email}</h1>
+// 		</>
+// 	)
+// }
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,39 +1,34 @@
-function Tweet() {
+import ProfileImage from './ProfileImage';
+import User from './User';
+import Timestamp from './Timestamp';
+import Message from './Message';
+import Actions from './Actions';
+
+
+function Tweet(props) {
   return (
     <div className="tweet">
-      <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
-        className="profile"
-        alt="profile"
-      />
-
+     
+    <ProfileImage image={props.tweet.user.image}/>
       <div className="body">
+
         <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
-
-          <span className="timestamp">Nov 30, 2020</span>
+          <User name={props.tweet.user.name}  handle={props.tweet.user.handle}/>
+          <Timestamp timestamp={props.tweet.timestamp} />
         </div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
+          <Message message={props.tweet.message} />
+        <Actions />
 
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i class="far fa-comment"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="far fa-heart"></i>
-          <i class="fas fa-share"></i>
         </div>
-      </div>
-
-      <i class="fas fa-ellipsis-h"></i>
+        <i class="fas fa-ellipsis-h"></i>
     </div>
   );
 }
 
 export default Tweet;
+
+
+
+
+
