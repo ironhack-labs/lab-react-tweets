7ec7cee145a29594263aa7d477d125328c8f3513
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-07-11 17:43:54 +0200

    done

--- src/App.js
+++ src/App.js
@@ -34,12 +34,13 @@ const tweetsArray = [
   },
 ];
 
-function App() {
+function App() { 
   return (
     <div className="App">
-      <Tweet />
+      <Tweet tweet = {tweetsArray[0]} />
+      <Tweet tweet = {tweetsArray[1]} />
+      <Tweet tweet = {tweetsArray[2]} />
     </div>
   );
 }
-
 export default App;
