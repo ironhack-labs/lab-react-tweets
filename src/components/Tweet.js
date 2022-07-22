91dd536e7ae89ef76e8871a4b5cafbb6fa7c4e60
ngc.918 <nicolas.g9182@gmail.com>
2022-07-22 11:41:09 -0400

    done

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,39 +1,24 @@
-function Tweet() {
-  return (
-    <div className="tweet">
-      <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
-        className="profile"
-        alt="profile"
-      />
+import Actions from "./Actions";
+import Message from "./Message";
+import ProfileImage from "./ProfileImage";
+import Timestamp from "./Timestamp";
+import User from "./User";
 
-      <div className="body">
-        <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
+function Tweet(props) {
+	return (
+		<div className="tweet">
+			<ProfileImage image={props.tweet.user.image} />
 
-          <span className="timestamp">Nov 30, 2020</span>
-        </div>
-
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i class="far fa-comment"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="far fa-heart"></i>
-          <i class="fas fa-share"></i>
-        </div>
-      </div>
-
-      <i class="fas fa-ellipsis-h"></i>
-    </div>
-  );
+			<div className="body">
+				<div className="top">
+					<User userData={props.tweet.user} />
+					<Timestamp time={props.tweet.timestamp} />
+				</div>
+				<Message message={props.tweet.message} />
+				<Actions />
+			</div>
+		</div>
+	);
 }
 
 export default Tweet;
