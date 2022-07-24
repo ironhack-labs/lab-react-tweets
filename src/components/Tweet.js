29230520ca1af73a888dd89102d1368d4f7f5826
Vale Quiroga <valentinqramos@outlook.com>
2022-07-24 16:48:49 -0500

    Iteration 9

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -2,6 +2,7 @@ import ProfileImage from './ProfileImage'
 import User from './User'
 import Timestamp from './Timestamp';
 import Message from './Message';
+import Actions from './Actions';
 
 function Tweet(props) {
   return (
@@ -13,16 +14,8 @@ function Tweet(props) {
           <User userData={props.tweet.user}/>
           <Timestamp time={props.tweet.timestamp}/>
         </div>
-
         <Message message={props.tweet.message}/>
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i class="far fa-comment"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="far fa-heart"></i>
-          <i class="fas fa-share"></i>
-        </div>
+        <Actions/>
       </div>
 
       <i class="fas fa-ellipsis-h"></i>
