bc02a2ba28295523bddebddcd584cfba0f06bf95
Ivona Pepic <ivona.pepic@gmail.com>
2022-07-25 18:11:37 +0200

    lab done

---
+++ src/components/Message.js
@@ -0,0 +1,8 @@
+function Message(props) {
+  // console.log(props.userData.name)
+  return (
+<p className="message"> {props.message} </p>
+  );
+}
+
+export default Message;
---
+++ src/components/Timestamp.js
@@ -0,0 +1,8 @@
+function Timestamp(props) {
+  // console.log(props.userData.name)
+  return (
+<span className="timestamp"> {props.time} </span>
+  );
+}
+
+export default Timestamp;
