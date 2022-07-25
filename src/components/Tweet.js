14e0bebd3a1a34df8323b6ccb3d748427d4cba62
Christoph86 <ChrstphBch@web.de>
2022-07-25 18:19:37 +0200

    Iteration3 -add Components

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,35 +1,31 @@
+import Actions from "./Actions";
+import Message from "./Message";
+import ProfileImage from "./ProfileImage";
+import Timestamp from "./Timestamp";
+import User from "./User";
+
 function Tweet(props) {
 
 
   return (
     <div className="tweet">
-      <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
-        className="profile"
-        alt="profile"
-      />
+      {/* profileImage here */}
+      <ProfileImage image={props.tweet.user.image} />
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">{props.tweet.user.name}</span>
-            <span className="handle">@{props.tweet.user.handle}</span>
-          </span>
+          {/* user here */}
+          <User user={props.tweet.user} />
 
-          <span className="timestamp">{props.tweet.timestamp}</span>
+          {/* timestamp here */}
+          <Timestamp time={props.tweet.timestamp} />
+
         </div>
+        {/* message here */}
+        <Message message={props.tweet.message} />
 
-        <p className="message">
-        {props.tweet.message}
-        </p>
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i class="far fa-comment"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="far fa-heart"></i>
-          <i class="fas fa-share"></i>
-        </div>
+        {/* actions here */}
+        <Actions />
       </div>
 
       <i class="fas fa-ellipsis-h"></i>
