074f0a5f86edd6189e16c64ea9b931abc3530f01
Andrea Restrepo <andrearestrepox@gmail.com>
2022-07-25 18:11:22 -0400

    done

--- src/App.js
+++ src/App.js
@@ -33,13 +33,14 @@ const tweetsArray = [
       "here is what. i plan to accomplish today: \n\n2. bark loudly. but at nothing \n7. lose my ball under the couch\n7b. politely ask the human. to get my ball\n3. immediately lose it again. under the same couch\n4. big nap. you have worked hard\n2. repeat",
   },
 ];
-
 function App() {
   return (
     <div className="App">
-      <Tweet />
+      <Tweet tweet={tweetsArray[0]} />
+      <Tweet tweet={tweetsArray[1]} />
+      <Tweet tweet={tweetsArray[2]} />
+
     </div>
   );
 }
-
 export default App;
---
+++ src/components/User.js
@@ -0,0 +1,14 @@
+
+function User(props){
+
+    const { userData } = props;
+  
+    return (
+      <span className="user">
+        <span className="name">{userData.name}</span>
+        <span className="handle">{userData.handle}</span>
+      </span>
+    );
+  }
+  
+  export default User;
