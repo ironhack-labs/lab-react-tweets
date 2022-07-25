91fbbeab72f8da623ac47775e8a39b6a94175651
Paul Tiry <tirypaul@gmail.com>
2022-07-25 17:46:44 +0200

    iteration 5 done

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,5 +1,6 @@
 import { FaRegComment, FaRetweet, FaRegHeart, FaShare, FaEllipsisH } from 'react-icons/fa'
 import ProfileImage from './ProfileImage'
+import User from './User'
 
 function Tweet({ tweet }) {
   return (
@@ -8,10 +9,7 @@ function Tweet({ tweet }) {
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">{tweet.user.name}</span>
-            <span className="handle">{`@${tweet.user.handle}`}</span>
-          </span>
+          <User userData={tweet.user} />
 
           <span className="timestamp">{tweet.timestamp}</span>
         </div>
