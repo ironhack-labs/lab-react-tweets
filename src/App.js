0f267e85e272bfe7705be80169fa513355673a87
Begum Eryildiz <begumeryildiz@gmail.com>
2022-07-25 19:11:59 +0200

    lab done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+
+
 import Tweet from "./components/Tweet";
 
 const tweetsArray = [
@@ -37,7 +39,9 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet />
+      <Tweet tweet={tweetsArray[0]} />
+      <Tweet tweet={tweetsArray[1]} />
+      <Tweet tweet={tweetsArray[2]} />
     </div>
   );
 }
