2488de83624ee5b1cb7ed730193245428b262548
Ihab Saleh <ihab.saleh@live.com>
2022-07-25 13:33:23 +0200

    done

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,39 +1,33 @@
-function Tweet() {
+import React from 'react';
+import User from "./User";
+import Timestamp from "./Timestamp";
+import Actions from "./Actions";
+import ProfileImage from "./ProfileImage"
+import Message from "./Message";
+
+
+
+
+
+function Tweet(props) {
   return (
     <div className="tweet">
-      <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
-        className="profile"
-        alt="profile"
-      />
+      <ProfileImage image={props.tweet.user.image} />
 
       <div className="body">
+        
         <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
-
-          <span className="timestamp">Nov 30, 2020</span>
+          <User userData={props.tweet.user} />
+          <Timestamp time={props.tweet.timestamp} />
         </div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i class="far fa-comment"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="far fa-heart"></i>
-          <i class="fas fa-share"></i>
-        </div>
+        <Message message={props.tweet.message} />
+        <Actions />
+        
       </div>
 
       <i class="fas fa-ellipsis-h"></i>
     </div>
   );
 }
-
 export default Tweet;
