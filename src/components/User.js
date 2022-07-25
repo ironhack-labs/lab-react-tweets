000ee5eaa0e8f4102bfd67e025b0f99dbce39b82
zubairmalek <zubairmalek95@gmail.com>
2022-07-25 18:06:11 +0100

    iterations 1-7 done

---
+++ src/components/User.js
@@ -0,0 +1,12 @@
+function User(props) {
+  console.log(props.userData);
+
+  return (
+    <span className="user">
+      <span className="name"> {props.userData.name} </span>
+      <span className="handle">@ {props.userData.handle}</span>
+    </span>
+  );
+}
+
+export default User;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
+    <link
+      rel="stylesheet"
+      href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+      integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
+      crossOrigin="anonymous"
+    />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
