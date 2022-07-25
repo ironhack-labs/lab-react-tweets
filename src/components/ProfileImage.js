6c9f63822ebc49f3bcd61a6cfc80785d871916cb
Darren Iyoyo <darreniyoyo@gmail.com>
2022-07-25 19:44:44 +0200

    done all iterations darren

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,15 @@
+function ProfileImage (props){
+    console.log(props);
+
+    return(
+        <>
+          <img
+        src={props.image}
+        className="profile"
+        alt="profile"
+      />
+        </>
+    )
+}
+
+export default ProfileImage;
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,34 +1,26 @@
-function Tweet() {
+import Actions from "../components/Actions";
+import Message from "../components/Message";
+import ProfileImage from "../components/ProfileImage";
+import Timestamp from "../components/Timestamp";
+import User from "../components/User";
+
+function Tweet(props) {
   return (
     <div className="tweet">
-      <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
-        className="profile"
-        alt="profile"
-      />
+    <ProfileImage image={props.tweet.user.image} />
 
-      <div className="body">
-        <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
+    <div className="body">
 
-          <span className="timestamp">Nov 30, 2020</span>
+      <div className="top">
+        <User userData={props.tweet.user} />
+        <Timestamp time={props.tweet.timestamp} />
         </div>
 
         <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
+        <Message message={props.tweet.message} /> 
         </p>
 
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i class="far fa-comment"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="far fa-heart"></i>
-          <i class="fas fa-share"></i>
-        </div>
+       <Actions />
       </div>
 
       <i class="fas fa-ellipsis-h"></i>
