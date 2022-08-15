76022bea5268ce8f8492c333fd7c0ac18902fff1
Adriana Leitao <adrianaleitao5_8@hotmail.com>
2022-08-15 16:31:03 +0100

    done

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function ProfileImage(props) {
+  const { image } = props;
+  return (
+    <div>
+    <img
+    src={image}
+    className="profile"
+    alt="profile"
+  />
+  </div>
+  )
+}
+
+export default ProfileImage;
