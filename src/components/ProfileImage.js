5a80258f97fbb07ef3ec96e9d7f57ba6644dc8ff
Iván Rodríguez <82571096+Ivan180687@users.noreply.github.com>
2022-08-27 17:36:54 +0200

    Done

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,13 @@
+function ProfileImage({ image }) {
+  return(
+    <div>
+      <img
+        src={image}
+        className="profile"
+        alt="profile"
+      />
+    </div>
+  )
+}
+
+export default ProfileImage
---
+++ src/components/Timestamp.js
@@ -0,0 +1,8 @@
+function Timestamp({ time }) {
+  return(
+    <div>
+      <span className="timestamp">{time}</span>
+    </div>
+  )
+}
+export default Timestamp
