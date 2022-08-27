88767addc3fdd4592be86be93b34357b145b1cee
Pablo <olbapdow@hotmail.com>
2022-08-28 01:14:47 +0200

    donegit status

---
+++ src/components/Message.js
@@ -0,0 +1,9 @@
+function Message({text}) {
+  return (
+    <p className="message">
+        {text}
+    </p>
+  );
+}
+
+export default Message;
---
+++ src/components/ProfileImage.js
@@ -0,0 +1,6 @@
+function ProfileImage({image}) {
+  return (
+    <img src={image} className="profile" alt="profile" />
+  )
+}
+export default ProfileImage;
