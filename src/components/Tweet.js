1bb36e47c570fedaf145deab3d44ffff930ccb89
Luis Urdaneta <luiseum95@gmail.com>
2022-08-29 20:55:36 +0200

    Iteration 2 - Done

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,11 +1,14 @@
 
 import './Tweet.css'
 
-function Tweet() {
+function Tweet({tweet}) {
+const  {user, timestamp, message} = tweet 
+const {name, image, handled} = user
+
   return (
     <div className="tweet">
       <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
+        src={image}
         className="profile"
         alt="profile"
       />
@@ -13,30 +16,37 @@ function Tweet() {
       <div className="body">
         <div className="top">
           <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
+            <span className="name">{name}</span>
+            <span className="handle">{handled}</span>
           </span>
 
-          <span className="timestamp">Nov 30, 2020</span>
+          <span className="timestamp">{timestamp}</span>
         </div>
 
         <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
+          {message}
         </p>
 
         <div className="actions">
           {/* Font Awesome icons */}
-          <i class="far fa-comment"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="far fa-heart"></i>
-          <i class="fas fa-share"></i>
+          <i className="far fa-comment"></i>
+          <i className="fas fa-retweet"></i>
+          <i className="far fa-heart"></i>
+          <i className="fas fa-share"></i>
         </div>
       </div>
 
-      <i class="fas fa-ellipsis-h"></i>
+      <i className="fas fa-ellipsis-h"></i>
     </div>
   );
 }
+
+Tweet.defaultProps ={
+  name:'Ironhack',
+  image:"https://i.imgur.com/9yw1Fyw.jpg",
+  handled: "@ironhack",
+  timestamp: "Nov 30, 2020",
+  message: 'On December 7th, we will be hosting a #webinar that will introduce you to #SQL! Are you ready? 🚀'
+  }
 
 export default Tweet;
