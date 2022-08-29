cf8bb53adeb4f5b0a617f80925b3e24c5411003f
Luis Urdaneta <luiseum95@gmail.com>
2022-08-29 21:47:31 +0200

    Iteration 9 - Done

--- src/App.js
+++ src/App.js
@@ -38,6 +38,9 @@ function App() {
   return (
     <div className="App">
       <Tweet tweet={ tweetsArray[0] }/>
+      <Tweet tweet={ tweetsArray[1] }/>
+      <Tweet tweet={ tweetsArray[2] }/>
+
     </div>
   );
 }
