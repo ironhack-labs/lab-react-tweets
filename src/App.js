bbc63101ec52267e98ab2dbfd68db43b8e038cf3
Alan ROGER <aa.roger404@gmail.com>
2022-08-29 17:01:06 +0200

    done

--- src/App.js
+++ src/App.js
@@ -38,6 +38,8 @@ function App() {
   return (
     <div className="App">
       <Tweet tweet={tweetsArray[0]} />
+      <Tweet tweet={tweetsArray[1]} />
+      <Tweet tweet={tweetsArray[2]} />
     </div>
   );
 }
