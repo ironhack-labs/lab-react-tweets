e3c758d29ff30d35ed3b17aa75bd6c988bf7ced7
cra123 <Onixmohamed@yahoo.fr>
2022-08-30 06:41:04 +0300

    done

---
+++ src/components/Message.js
@@ -0,0 +1,12 @@
+
+function Message(props) {
+    return (
+        <div>
+            <p className="message">
+                {props.message}
+            </p>
+        </div>
+    )
+}
+
+export default Message
---
+++ src/components/ProfileImage.js
@@ -0,0 +1,16 @@
+
+
+function ProfileImage(props) {
+  return (
+    <div>
+          <img
+              // src="https://i.imgur.com/9yw1Fyw.jpg"
+              src={props.image}
+              className="profile"
+              alt="profile"
+          />
+    </div>
+  );
+}
+
+export default ProfileImage;
