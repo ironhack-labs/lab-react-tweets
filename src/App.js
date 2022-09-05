8aa910b764790358533b95049b2f313759db7397
Chlotte <charlottealleaume99@gmail.com>
2022-09-05 16:20:46 +0200

    finished lab

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet />
+      {tweetsArray.map(tweet => <Tweet key={tweet.timestamp} tweet={tweet}/>)} 
     </div>
   );
 }
