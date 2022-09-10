b839273f3c1ab3391732f776c95be9c8e9b736bb
Stefano Silva <stefanodavi1sx@gmail.com>
2022-09-09 21:39:12 -0300

    iterations 4 and 5 done

---
+++ src/components/User.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+
+function User(props) {
+    return (
+        <span className="user">
+             <span className="name">  {props.userData.name} </span>
+            <span className="handle">@{props.userData.handle}</span>
+        </span>
+        );
+}
+
+export default User;
