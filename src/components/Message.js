fd6f4c8430b89188d5f15814fed926413da130d2
poncho678 <vincent.tollens@googlemail.com>
2022-09-15 20:47:33 +0200

    done

---
+++ src/components/Message.js
@@ -0,0 +1,5 @@
+function Message({ message }) {
+  return <p className="message">{message}</p>;
+}
+
+export default Message;
---
+++ src/components/Timestamp.js
@@ -0,0 +1,5 @@
+function Timestamp({ timestamp }) {
+  <span className="timestamp">{timestamp}</span>;
+}
+
+export default Timestamp;
