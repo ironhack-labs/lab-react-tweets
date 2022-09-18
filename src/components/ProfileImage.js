0cec63f48e872ec9c302c2fba26a5505aa1b8c2e
Abhijeet Dalvi <abhijeetvdalvi@.com>
2022-09-18 15:46:06 +0200

    done

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function ProfileImage(props) {
+  const { image } = props;
+  return <img src={image} className="profile" alt="profile" />;
+}
+
+export default ProfileImage;
---
+++ src/components/Timestamp.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Timestamp(props) {
+  const { time } = props;
+  return <span className="timestamp">{time}</span>;
+}
+
+export default Timestamp;
