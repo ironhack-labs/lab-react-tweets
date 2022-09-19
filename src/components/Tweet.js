4598bb21c5342a80319b6effae167137a934dbe1
Kenneth Elumba <kennethelumba@gmail.com>
2022-09-19 20:32:17 +0200

    add user component data

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,4 +1,5 @@
 import ProfileImage from './ProfileImage'
+import User from './User'
 
 function Tweet(props) {
   // console.log(props.tweet.user.name);
@@ -9,10 +10,8 @@ function Tweet(props) {
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">{props.tweet.user.name}</span>
-            <span className="handle">{props.tweet.user.handle}</span>
-          </span>
+          
+        <User userData={props.tweet.user} />
 
           <span className="timestamp">{props.tweet.timestamp}</span>
         </div>
