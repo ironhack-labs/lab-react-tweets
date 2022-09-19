b6e160dc410458a62c2aaa7623cd27ada3b8185e
Jpta24 <jeanpierretorres@gmail.com>
2022-09-19 18:16:36 +0200

    Lab Tweet Done

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,12 @@
+
+const ProfileImage = ({data}) => {
+  return (
+    <img
+        src={data}
+        className="profile"
+        alt="profile"
+      />
+  )
+}
+
+export default ProfileImage
---
+++ src/components/Timestamp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Timestamp = ({time}) => {
+  return (
+    <span className="timestamp">{time}</span>
+  )
+}
+
+export default Timestamp
