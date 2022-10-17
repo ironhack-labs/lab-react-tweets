796d0e0725cad65e626ca784c7401fbd5e1b8300
berkayyildirim <huseyinberkayyildirim@gmail.com>
2022-10-17 19:53:37 +0300

    complete all iterations

--- src/App.js
+++ src/App.js
@@ -36,8 +36,10 @@ const tweetsArray = [
 
 function App() {
   return (
-    <div className="App">
-      <Tweet />
+    <div className='App'>
+      <Tweet tweet={tweetsArray[0]} />
+      <Tweet tweet={tweetsArray[1]} />
+      <Tweet tweet={tweetsArray[2]} />
     </div>
   );
 }
