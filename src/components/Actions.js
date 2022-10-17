24bd3c724236d82f1d723e9695aa8a3aa6b2b0a6
Julia Spriggs <ironhack@julia-spriggs.fr>
2022-10-17 18:42:48 +0200

    finished iteration 8

---
+++ src/components/Actions.js
@@ -0,0 +1,13 @@
+function Actions() {
+    return (
+        <div className="actions">
+            {/* Font Awesome icons */}
+            <i className="far fa-comment" />
+            <i className="fas fa-retweet" />
+            <i className="far fa-heart" />
+            <i className="fas fa-share" />
+        </div>
+    )
+}
+
+export default Actions;
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -2,29 +2,22 @@ import ProfileImage from "./ProfileImage";
 import User from "./User";
 import Timestamp from "./Timestamp";
 import Message from "./Message";
+import Actions from "./Actions";
 
 function Tweet(props) {
   const tweet = props.tweet;
   const user = tweet.user;
   return (
     <div className="tweet">
+    
       <ProfileImage imageURL={user.image} />
-
       <div className="body">
         <div className="top">
           <User details={user} />
           <Timestamp time={tweet.timestamp} />
         </div>
-
         <Message text={tweet.message} />
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment"></i>
-          <i className="fas fa-retweet"></i>
-          <i className="far fa-heart"></i>
-          <i className="fas fa-share"></i>
-        </div>
+        <Actions />
       </div>
 
       <i className="fas fa-ellipsis-h"></i>
