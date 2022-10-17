0c6f6a8a072509f1dbd92282ae2fb28856f867d9
julianBrandon <p0lterg3istt@gmail.com>
2022-10-17 18:38:46 +0200

    Add iteration 5

--- src/components/User.js
+++ src/components/User.js
@@ -1,5 +1,11 @@
-function User() {
-
+function User(props) {
+    console.log(props);
+    return (
+        <span className="user">
+            <span className="name">{props.userData.name}</span>
+            <span className="handle">{props.userData.handle}</span>
+        </span>
+    );
 }
 
 export default User;
