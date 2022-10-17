a5c54314c9c216fdc83dbb54f7dbff8d3e9b0e16
VKhodyrev <vkhodyrev1@gmail.com>
2022-10-17 19:55:42 +0300

    iiterations 1 2 done

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,4 +1,6 @@
-function Tweet() {
+function Tweet(props) {
+
+  console.log(props);
   return (
     <div className="tweet">
       <img
@@ -10,28 +12,28 @@ function Tweet() {
       <div className="body">
         <div className="top">
           <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
+            <img src={props.tweet.user.image} alt="User"/>
+            <span className="name">{props.tweet.user.name}</span>
+            <span className="handle">{props.tweet.user.handle}</span>
           </span>
 
-          <span className="timestamp">Nov 30, 2020</span>
+          <span className="timestamp">{props.tweet.timestamp}</span>
         </div>
 
         <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
+          {props.tweet.message}
         </p>
 
         <div className="actions">
           {/* Font Awesome icons */}
-          <i class="far fa-comment"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="far fa-heart"></i>
-          <i class="fas fa-share"></i>
+          <i className="far fa-comment"></i>
+          <i className="fas fa-retweet"></i>
+          <i className="far fa-heart"></i>
+          <i className="fas fa-share"></i>
         </div>
       </div>
 
-      <i class="fas fa-ellipsis-h"></i>
+      <i className="fas fa-ellipsis-h"></i>
     </div>
   );
 }
