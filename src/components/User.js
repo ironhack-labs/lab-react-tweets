aae389c75d87026053584353c1c3dba4bd57996b
HaridhaRavi <haridha.gr@gmail.com>
2022-10-17 18:35:56 +0200

    lab done

---
+++ src/components/User.js
@@ -0,0 +1,10 @@
+function User(props) {
+  console.log(props);
+  return (
+    <span className="user">
+      <span className="name">{props.userData.name}</span>
+      <span className="handle">{props.userData.handle}</span>
+    </span>
+  );
+}
+export default User;
