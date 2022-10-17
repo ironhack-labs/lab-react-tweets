09298824e43580ae54289b57bcb8812fa8d41639
Farzin khaledi <Khaledifarzin1@gmail.com>
2022-10-17 22:40:24 +0100

    solved

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,4 @@
+function ProfileImage(props) {
+  return <img src={props.image} alt="profile" className="profile" />;
+}
+export default ProfileImage;
---
+++ src/components/User.js
@@ -0,0 +1,10 @@
+import '../App.css';
+function User(props) {
+  return (
+    <span className="user">
+      <span className="name">{props.userData.name}</span>
+      <span className="handle">{props.userData.handle}</span>
+    </span>
+  );
+}
+export default User;
