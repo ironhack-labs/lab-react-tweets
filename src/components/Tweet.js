e8e491fee0ee6ae464519d5e7ef0717e57385dc7
Gabriel Gomez <ggomez.box@gmail.com>
2022-10-17 17:37:05 +0200

    iteration 1 done

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,4 +1,5 @@
-function Tweet() {
+function Tweet({ tweet, children }) {
+  console.log(tweet);
   return (
     <div className="tweet">
       <img
@@ -14,24 +15,22 @@ function Tweet() {
             <span className="handle">@ironhack</span>
           </span>
 
-          <span className="timestamp">Nov 30, 2020</span>
+          {/* <span className="timestamp">Nov 30, 2020</span> */}
+          <span className="timestamp">{tweet.timestamp}</span>
         </div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
+        <p className="message">{tweet.message} 🚀</p>
 
         <div className="actions">
           {/* Font Awesome icons */}
-          <i class="far fa-comment"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="far fa-heart"></i>
-          <i class="fas fa-share"></i>
+          <i className="far fa-comment"></i>
+          <i className="fas fa-retweet"></i>
+          <i className="far fa-heart"></i>
+          <i className="fas fa-share"></i>
         </div>
       </div>
 
-      <i class="fas fa-ellipsis-h"></i>
+      <i className="fas fa-ellipsis-h"></i>
     </div>
   );
 }
