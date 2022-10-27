937fa16ba1528671ac1188fc14d53cec99e3d118
jdlms <97018563+jdlms@users.noreply.github.com>
2022-10-27 22:44:11 +0200

    final commit

---
+++ src/components/Actions.js
@@ -0,0 +1,13 @@
+function Actions() {
+  return (
+    <>
+      {/* Font Awesome icons */}
+      <i className="far fa-comment" />
+      <i className="fas fa-retweet"></i>
+      <i className="far fa-heart"></i>
+      <i className="fas fa-share"></i>
+    </>
+  );
+}
+
+export default Actions;
---
+++ src/components/ProfileImage.js
@@ -0,0 +1,5 @@
+function ProfileImage({ userImg }) {
+  return <img src={userImg} className="profile" alt="profile" />;
+}
+
+export default ProfileImage;
