2dd8f4291d32edd5baf5e3c545e3d3ed8f3404b6
Pedro Pedrosa <prpedrosa95@gmail.com>
2022-11-14 15:04:46 +0000

    finishes lab

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ProfileImage(props) {
+    const {image} = props
+  return (
+    
+        <img src={image} className="profile" alt="profile"/>
+    
+  )
+}
+
+export default ProfileImage
