7f29c7990c96efba487591c73a937d241a89b43d
Raiza Garcia <raizagarcias@gmail.com>
2022-11-14 18:17:05 +0000

    done

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function ProfileImage(props) {
+    const {image} = props;
+    return (
+        <div> <img src={image} className="profile" alt="profile"/> </div>
+    )
+}
+
+export default ProfileImage;
