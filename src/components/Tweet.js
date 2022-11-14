e81d2aeac9540db54513cb66b2d4e52e056ec162
John Doe <atarod.shirin@gmail.com>
2022-11-14 17:07:09 +0100

    it2 done

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,4 +1,4 @@
-function Tweet() {
+function Tweet({tweet}) {
   return (
     <div className="tweet">
       <img
@@ -10,16 +10,15 @@ function Tweet() {
       <div className="body">
         <div className="top">
           <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
+            <span className="name">{tweet.user.name}</span>
+            <span className="handle">{tweet.user.handle}</span>
           </span>
 
-          <span className="timestamp">Nov 30, 2020</span>
+          <span className="timestamp">{tweet.timestamp}</span>
         </div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
+       <p className="message">
+        {tweet.message}
         </p>
 
         <div className="actions">
