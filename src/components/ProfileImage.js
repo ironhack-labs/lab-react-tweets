a4bef0eba4419317530c3232743f1c96c0ed0061
Melissa Imai <melissayuri_903@hotmail.com>
2022-11-28 19:51:21 +0100

    lab done

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,13 @@
+function ProfileImage(props) {
+  return (
+    <div className="profileImage">
+      <img
+        src={props.image}
+        className="profile"
+        alt="profile"
+      />
+    </div>
+  )
+}
+
+export default ProfileImage;
---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+const User = (props) => {
+  const { name, handle } = props.userData
+  return (
+    <span className="user">
+      <span className="name">{name}</span>
+      <span className="handle">{handle}</span>
+    </span>
+  )
+}
+
+export default User;
