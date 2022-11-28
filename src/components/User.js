98ea1c1c78d92abb4b9ffa0b9dc32ac44ca4595b
Carolina Acuna <carolina.acuna90@gmail.com>
2022-11-29 00:18:00 +0100

    done

--- src/components/User.js
+++ src/components/User.js
@@ -2,8 +2,8 @@ function User(props) {
     return (
         <>
         <span className="user">
-            <span className="handle">{props.handle}</span>
             <span className="name">{props.name}</span>
+            <span className="handle">{props.handle}</span>
         </span>
         </>
     )
