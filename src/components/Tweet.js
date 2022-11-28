830a724ad88670562dd2a03c595e5279eed21903
Costache Robert Valentin <costache.robert.valentin@gmail.com>
2022-11-28 02:44:02 +0100

    Iteration 8 | Actions Component

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,49 +1,31 @@
+// Import Components 
+import ProfileImage from "./ProfileImage";
+import User from "./User";
+import Timestamp from  "./Timestamp";
+import Message from "./Message";
+import Actions from "./Actions";
+
 function Tweet(props) {
-  // const tweetsArray = [
-  //   {
-  //     user: {
-  //       name: "Thoughts of Dog®",
-  //       image: "https://i.imgur.com/b0EdHVV.jpg",
-  //       handle: "dog_feelings",
-  //     },
-  //     timestamp: "1h ago",
-  //     message:
-  //       "the human likes to say. that i live here rent free. but i would argue. this housing accommodation. is my payment. for a lifetime of love. and excellent company",
-  //   },
-  // const { name } = this.props.user;
+
   console.log(props.tweet.user);
   const  {name, image, handle} = props.tweet.user;
   const {timestamp, message} = props.tweet;
 
   return (
     <div className="tweet">
-      <img
-        src={image}
-        className="profile"
-        alt="profile"
-      />
 
+    <ProfileImage image={image} />
+    
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">{name}</span>
-            <span className="handle">{handle}</span>
-          </span>
-
-          <span className="timestamp">{timestamp}</span>
+          <User name={name} handle={handle} />
+          <Timestamp time={timestamp} />
+         
         </div>
 
-        <p className="message">
-           {message}
-        </p>
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment"></i>
-          <i className="fas fa-retweet"></i>
-          <i className="far fa-heart"></i>
-          <i className="fas fa-share"></i>
-        </div>
+        <Message message={message} />
+       
+        <Actions />
       </div>
 
       <i className="fas fa-ellipsis-h"></i>
