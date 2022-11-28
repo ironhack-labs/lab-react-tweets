5cf91e56182708037bc53630c8d3e78071772da7
Guilherme Muniz <guilhermemazzolini@hotmail.com>
2022-11-28 16:05:05 +0100

    Guilherme Muniz Lab done!

---
+++ src/components/User.js
@@ -0,0 +1,9 @@
+function User({userData}) {
+    console.log(userData)
+    return (<span className="user">
+            <span className="name"> {userData.name} </span>
+            <span className="handle"> @{userData.handle} </span>
+            </span>)
+}
+
+export default User
