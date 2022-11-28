9617c88f7042ecebcd5b6dc135cc447f5a4e88bc
YanxiaWu <yanxiawusilva@gmail.com>
2022-11-28 16:34:33 +0100

    Done

---
+++ src/components/Message.jsx
@@ -0,0 +1,8 @@
+const Message = props => {
+    const { message } = props
+    return (
+        <p className="message"> {message}</p>
+    )
+}
+
+export default Message
---
+++ src/components/ProfileImage.jsx
@@ -0,0 +1,7 @@
+const ProfileImage = props => {
+    const { image } = props
+    return (
+        <img src={image} className='profile' alt='profile'></img>
+    )
+}
+export default ProfileImage
