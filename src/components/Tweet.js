a9ab27c9cd95e76c5cf34901358723f6a901bc2c
Ervin Varga <vargaervinerno0@gmail.comgit>
2022-11-28 15:43:07 +0100

    finished lab

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,34 +1,32 @@
-function Tweet() {
+import ProfileImage from "./ProfileImage";
+import User from "./User";
+import Actions from "./Actions";
+import Timestamp from "./Timestamp";
+import Message from "./Message";
+
+function Tweet(props) {
   return (
     <div className="tweet">
-      <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
-        className="profile"
-        alt="profile"
-      />
+      <ProfileImage image={props.tweet.user.image} />
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
-
+          <User 
+          userData = {props.tweet.user}
+            
+          />
+          <Timestamp
+            Timestamp={props.tweet.timestamp}
+          />
           <span className="timestamp">Nov 30, 2020</span>
         </div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
+        <Message
+          Message={props.tweet.message}
+        />
 
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment"></i>
-          <i className="fas fa-retweet"></i>
-          <i className="far fa-heart"></i>
-          <i className="fas fa-share"></i>
-        </div>
+        <Actions
+        />
       </div>
 
       <i className="fas fa-ellipsis-h"></i>
