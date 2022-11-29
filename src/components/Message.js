cf20050f808d8645f7fe9cb37140dec2162422b4
Frances Morales Velilla <frances0688@gmail.com>
2022-11-29 08:13:22 +0100

    Frances Morales - done

---
+++ src/components/Message.js
@@ -0,0 +1,11 @@
+function Message (props) {
+    return (
+        <div>
+            <p className="message">
+            {props.message}
+            </p>
+        </div>
+    );
+}
+
+export default Message;
---
+++ src/components/User.js
@@ -0,0 +1,12 @@
+function User (props) {
+    return (
+        <div>
+            <span className="user">
+                <span className="name">{props.userData.name}</span>
+                <span className="handle">{props.userData.handle}</span>
+            </span>
+        </div>
+    );
+}
+
+export default User;
