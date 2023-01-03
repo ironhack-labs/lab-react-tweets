73c20d8c1e412e8e16058a27b2d878927f7b4c80
Alfie Collins <alfiecollins44@hotmail.co.uk>
2023-01-03 11:58:25 +0000

    done

---
+++ src/components/Actions.js
@@ -0,0 +1,15 @@
+const Actions = (props) => {
+    return(
+        <div className="actions">
+<i class="far fa-comment"></i>
+<i class="fas fa-retweet"></i>
+<i class="far fa-heart"></i>
+<i class="fas fa-share"></i>
+</div>
+    )
+}
+
+export default Actions
+
+
+
