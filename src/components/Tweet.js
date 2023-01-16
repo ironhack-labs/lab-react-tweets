5d62bd44ecacb55f1904a7db84841ac0d72d62b0
Lovro Boric <lovro.boric@gmail.com>
2023-01-16 18:43:55 +0100

    prettified Tweert

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,25 +4,19 @@ import Message from "./Message";
 import Actions from "./Actions";
 import Timestamp from "./Timestamp";
 
-
 function Tweet({ values }) {
   return (
     <div className="tweet">
-
       <ProfileImage image={values.user.image} />
 
-
       <div className="body">
-
         <div className="top">
           <User userData={values.user} />
           <Timestamp time={values.timestamp} />
         </div>
 
         <Message message={values.message} />
-
         <Actions />
-
       </div>
 
       <i className="fas fa-ellipsis-h"></i>
