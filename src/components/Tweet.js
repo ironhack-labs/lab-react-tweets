0cc46f4b2ceec36b9f3e5c6acaee6421d874b368
Lai In Chang <lavender880331@gmail.com>
2023-01-16 17:12:21 +0100

    finish iteration 3-4

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,12 +1,9 @@
+import ProfileImage from "./ProfileImage";
+
 function Tweet(props) {
-  console.log(props.tweet);
   return (
     <div className="tweet">
-      <img
-        src={props.tweet.user.image}
-        className="profile"
-        alt="profile"
-      />
+      <ProfileImage image={props.tweet.user.image} />
 
       <div className="body">
         <div className="top">
