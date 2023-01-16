c9cb77b496f74ab836a33fba6d83458d8702dd86
Lai In Chang <lavender880331@gmail.com>
2023-01-16 17:34:44 +0100

    finish iteration 5-7

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,4 +1,7 @@
 import ProfileImage from "./ProfileImage";
+import User from "./User";
+import Timestamp from "./Timestamp";
+import Message from "./Message";
 
 function Tweet(props) {
   return (
@@ -7,17 +10,13 @@ function Tweet(props) {
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">{props.tweet.user.name} </span>
-            <span className="handle">@{props.tweet.user.handle}</span>
-          </span>
+          
+          <User userData={props.tweet.user} />
 
-          <span className="timestamp">{props.tweet.timestamp}</span>
+          <Timestamp time={props.tweet.timestamp} />
         </div>
 
-        <p className="message">
-          {props.tweet.message}
-        </p>
+        <Message message={props.tweet.message} /> 
 
         <div className="actions">
           {/* Font Awesome icons */}
