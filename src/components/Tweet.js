9be5b4e8d1172296d719806ca365e478325e7241
David Mullally <mullallydavid7@gmail.com>
2023-01-16 22:12:31 +0100

    fix display problem

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,28 +1,29 @@
 import ProfileImage from "./ProfileImage";
 import User from "./User";
 import Timestamp from "./Timestamp";
-import Message from './Message';
+import Message from "./Message";
 import Actions from "./Actions";
-
 
 function Tweet(props) {
   return (
-    <div>
+    <>
       {props.array.map((tweet) => {
-        return <div className="Tweet"key={tweet.user.id}>
-        <ProfileImage image={tweet.user.image} />
-      <div className="body">
-      "<div className="top">
-          <User userData={tweet.user} />
-          <Timestamp time={tweet.timestamp} />
-        </div>
-        <Message message={tweet.message} /> 
-        <Actions />
-      </div>
-          <i className="fas fa-ellipsis-h"></i>
-          </div>   
+        return (
+          <div className="tweet">
+            <ProfileImage image={tweet.user.image} />
+            <div className="body">
+              <div className="top">
+                <User userData={tweet.user} />
+                <Timestamp time={tweet.timestamp} />
+              </div>
+              <Message message={tweet.message} />
+              <Actions />
+            </div>
+            <i class="fas fa-ellipsis-h"></i>
+          </div>
+        );
       })}
-    </div>
+    </>
   );
 }
 
