8db34bc51267d445231d94d8e3ce29c7315439fd
David Hatch <hatchdavidv@gmail.com>
2023-01-28 10:23:37 -0600

    Se añade estructura del tuit

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -2,7 +2,19 @@ function Tweet() {
     return (
       <div className="tweet">
         <ProfileImage image={props.tweet.user.image} />
+        <div className="body">
         
+        <div className="top">
+          <User userData={props.tweet.user} />
+          <Timestamp time={props.tweet.timestamp} />
+        </div>
+
+        <Message message={props.tweet.message} />
+        <Actions />
+        
+      </div>
+
+      <i class="fas fa-ellipsis-h"></i>
 
       </div>
     );
