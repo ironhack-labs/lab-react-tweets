a6d1eaade59cf92d8506e5caf2d66eddffa1fd0c
Jo Huerta <santiago@alimx.mx>
2023-02-04 14:20:16 -0600

    WDFT-MEX-AGOSTO-JoseEzequielHuerta

---
+++ src/components/Timestamp.js
@@ -0,0 +1,8 @@
+function Timestamp(props){
+    return(
+        <>
+            <span className="timestamp"> {props.times} </span>
+        </>        
+    );
+}
+export default Timestamp;
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,39 +1,30 @@
-function Tweet() {
-  return (
+import User from './User';
+import ProfileImage from './ProfileImage';
+import Timestamp from './Timestamp';
+import Message from './Message';
+import Actions from './Actions';
+
+function Tweet(props) {
+  const {user,timestamp,message} = props.tweet;
+  return(
     <div className="tweet">
-      <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
-        className="profile"
-        alt="profile"
-      />
+      <ProfileImage image={user.image}/>
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
 
-          <span className="timestamp">Nov 30, 2020</span>
+          <User userData={user}/>
+            <Timestamp time={timestamp}/>
         </div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i class="far fa-comment"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="far fa-heart"></i>
-          <i class="fas fa-share"></i>
-        </div>
+        <Message message={message} />   
+        <Actions/>
       </div>
 
-      <i class="fas fa-ellipsis-h"></i>
+      <i className="fas fa-ellipsis-h"></i>
     </div>
   );
 }
+
 
 export default Tweet;
