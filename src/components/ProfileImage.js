71745216166ac1abb94de0ec726f4581354f2917
Nadine Machmeier <info@nadinemachmeier.com>
2023-02-16 21:34:36 +0000

    lab done

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,10 @@
+function ProfileImage(props) {
+
+    return(
+        <div>
+           <img src={props.image} className="profile" alt="profile"/> 
+        </div>
+    )
+}
+
+export default ProfileImage
