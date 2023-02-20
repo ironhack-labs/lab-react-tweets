29492e7018bcd45aa1ef53b4a72808fde734cb48
gulsahbayazit <bayazitgulsahbayazit@gmail.com>
2023-02-20 17:59:05 +0100

    [FTWD]

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Tweet from "./components/Tweet";
+import React from "react";
 
 const tweetsArray = [
   {
@@ -37,7 +38,9 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet />
+      <Tweet tweet={tweetsArray[0]} />
+      <Tweet tweet={tweetsArray[1]} />
+      <Tweet tweet={tweetsArray[2]} />
     </div>
   );
 }
