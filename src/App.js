4a599cef370f4e58c26b2cb5ef85500010505414
Neilly <neilpilarca@gmail.com>
2023-02-20 15:25:43 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -37,7 +37,14 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet />
+      <Tweet
+        user={{
+          name: tweetsArray[0].user.name,
+          image: tweetsArray[0].user.image,
+          handle: tweetsArray[0].user.handle,
+        }}
+        message={tweetsArray[0].message}
+      />
     </div>
   );
 }
