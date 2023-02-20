62bcd73303baa05742e7ddcd8f3757cb22beb845
Tobias Krauss <tobias.tk.krauss@gmail.com>
2023-02-20 16:36:00 +0100

    Iteration 7

---
+++ src/components/Message.js
@@ -0,0 +1,9 @@
+function Message(props) {
+    return (
+         <p className="message">
+    "the human likes to say. that i live here rent free. but i would argue. this housing accommodation. is my payment. for a lifetime of love. and excellent company"
+  </p>
+   );
+}
+
+export default Message;
