da2f8cc5ecd1040676aad9da5befdbd76f6e85d6
Tobias Krauss <tobias.tk.krauss@gmail.com>
2023-02-20 16:31:32 +0100

    Iteration 5

---
+++ src/components/User.js
@@ -0,0 +1,10 @@
+function User(props) {
+    return ( 
+        <span className="user">
+            <span className="name">"Thoughts of Dog®"</span>
+            <span className="handle">"dog_feelings"</span>
+          </span>
+     );
+}
+
+export default User;
