e2dc6187224166ddf7266834f4aa29f9f0dd3e96
waggnerl <wagnermflira@gmail.com>
2023-02-27 12:54:33 +0000

    chore: Remove unnecessary component structure

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -5,23 +5,21 @@ import Message from "./Message";
 import Actions from "./Actions";
 
 export default function Tweet({ tweet }) {
-  return tweet.map((el) => {
-    return (
-      <div className="tweet">
-        <ProfileImage image={el.user.image} />
+  return tweet.map((el) => (
+    <div className="tweet">
+      <ProfileImage image={el.user.image} />
 
-        <div className="body">
-          <div className="top">
-            <User userData={el.user} />
-            <Timestamp time={el.timestamp} />
-          </div>
-
-          <Message message={el.message} />
-          <Actions />
+      <div className="body">
+        <div className="top">
+          <User userData={el.user} />
+          <Timestamp time={el.timestamp} />
         </div>
 
-        <i class="fas fa-ellipsis-h"></i>
+        <Message message={el.message} />
+        <Actions />
       </div>
-    );
-  });
+
+      <i class="fas fa-ellipsis-h"></i>
+    </div>
+  ));
 }
