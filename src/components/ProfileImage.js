4064fd6476d7505f2d41788d057f947b22fb295f
EmilianoCC <emiliano93cc@gmail.com>
2023-02-27 02:23:00 -0600

    Commit :D

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,9 @@
+function ProfileImage(props) {
+  return (
+    <div>
+      <img src={props.image} className='profile' alt='' />
+    </div>
+  )
+}
+
+export default ProfileImage
---
+++ src/components/Timestamp.js
@@ -0,0 +1,9 @@
+function Timestamp(props) {
+  return (
+    <div>
+      <span className='timestamp'> {props.timestamp} </span>
+    </div>
+  )
+}
+
+export default Timestamp
