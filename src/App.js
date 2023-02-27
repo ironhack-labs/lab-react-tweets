f9bf9d14c698a9fe07719aee112ee48deb3c0eee
Alberto Gómez <albertogomrod5@gmail.com>
2023-02-27 17:16:05 +0100

    Lab done

--- src/App.js
+++ src/App.js
@@ -37,8 +37,12 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet />
+      <Tweet tweet= {tweetsArray[0]} />
+      <Tweet tweet= {tweetsArray[1]} />
+      <Tweet tweet= {tweetsArray[2]} />
+
     </div>
+    
   );
 }
 
