ec448fca86df51dea8cd06d243f057d5cad128f7
Vanessa Vieira <vanessafvieira22@gmail.com>
2023-02-27 16:09:09 +0000

    solved

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Tweet from "./components/Tweet";
 
+
 const tweetsArray = [
   {
     user: {
@@ -34,10 +35,12 @@ const tweetsArray = [
   },
 ];
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-      <Tweet />
+      <Tweet tweet={ tweetsArray[0] } />
+      <Tweet tweet={ tweetsArray[1] } />
+      <Tweet tweet={ tweetsArray[2] } />
     </div>
   );
 }
