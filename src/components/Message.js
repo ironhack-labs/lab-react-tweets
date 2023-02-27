048f582e0428bc8054c5d845e9f7b256a766f4e2
Tiago Rato <tiagorato13@hotmail.com>
2023-02-27 16:13:01 +0000

    submit lab

---
+++ src/components/Message.js
@@ -0,0 +1,6 @@
+function Message(props) {
+  /* console.log(props); */
+  return <p className="message"> {props.message} </p>;
+}
+
+export default Message;
---
+++ src/components/ProfileImage.js
@@ -0,0 +1,5 @@
+function ProfileImage(props) {
+  /* console.log(props); */
+  return <img src={props.image} className="profile" alt="profile" />;
+}
+export default ProfileImage;
