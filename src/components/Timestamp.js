2dee16a467aa6b4b7218f92a1203828e827f71d7
romi <romi.varnier@gmail.com>
2023-02-27 17:59:51 +0100

    Done

---
+++ src/components/Timestamp.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Timestamp = ({ timestamp }) => {
+  return <span className="timestamp">{timestamp}</span>
+}
+
+export default Timestamp
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,39 +1,29 @@
-function Tweet() {
+import React from 'react'
+import Actions from '../components/Actions'
+import Timestamp from './Timestamp'
+import User from './User'
+import Message from './Message'
+import ProfileImage from './ProfileImage'
+
+const Tweet = ({ tweet }) => {
+  // console.log(tweet)
   return (
     <div className="tweet">
-      <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
-        className="profile"
-        alt="profile"
-      />
+      <ProfileImage image={tweet.user.image} />
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
-
-          <span className="timestamp">Nov 30, 2020</span>
+          <User user={tweet.user} />
+          <Timestamp timestamp={tweet.timestamp} />
         </div>
+        <Message message={tweet.message} />
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment"></i>
-          <i className="fas fa-retweet"></i>
-          <i className="far fa-heart"></i>
-          <i className="fas fa-share"></i>
-        </div>
+        <Actions />
       </div>
 
       <i className="fas fa-ellipsis-h"></i>
     </div>
-  );
+  )
 }
 
-export default Tweet;
+export default Tweet
