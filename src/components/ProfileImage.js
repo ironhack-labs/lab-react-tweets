99ee05374c696f9da9633dd171fbc11aed5400a3
waggnerl <wagnermflira@gmail.com>
2023-02-27 12:52:30 +0000

    feat: Include components

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ProfileImage({ image }) {
+  return <img src={image} className="profile" alt="profile" />;
+}
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,39 +1,27 @@
-function Tweet() {
-  return (
-    <div className="tweet">
-      <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
-        className="profile"
-        alt="profile"
-      />
+import ProfileImage from "./ProfileImage";
+import Timestamp from "./Timestamp";
+import User from "./User";
+import Message from "./Message";
+import Actions from "./Actions";
 
-      <div className="body">
-        <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
+export default function Tweet({ tweet }) {
+  return tweet.map((el) => {
+    return (
+      <div className="tweet">
+        <ProfileImage image={el.user.image} />
 
-          <span className="timestamp">Nov 30, 2020</span>
+        <div className="body">
+          <div className="top">
+            <User userData={el.user} />
+            <Timestamp time={el.timestamp} />
+          </div>
+
+          <Message message={el.message} />
+          <Actions />
         </div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment"></i>
-          <i className="fas fa-retweet"></i>
-          <i className="far fa-heart"></i>
-          <i className="fas fa-share"></i>
-        </div>
+        <i class="fas fa-ellipsis-h"></i>
       </div>
-
-      <i className="fas fa-ellipsis-h"></i>
-    </div>
-  );
+    );
+  });
 }
-
-export default Tweet;
