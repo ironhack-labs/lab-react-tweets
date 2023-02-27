117b9fb902b968ebc4b39a89ad43ac2c60209601
Joanadgomes <joanadiogog@gmail.com>
2023-02-27 14:57:56 +0000

    solved

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,34 +1,28 @@
-function Tweet() {
+import ProfileImage from "./ProfileImage";
+import Timestamp from "./Timestamp";
+import User from "./User";
+import Message from "./Message";
+import Actions from "./Actions"
+
+function Tweet(props) {
+  console.log(props);
+
+  const {tweet} = props;
+
   return (
     <div className="tweet">
-      <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
-        className="profile"
-        alt="profile"
+      <ProfileImage image={tweet.user.image}
       />
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
+          <User userData={tweet.user}/>
+          <Timestamp time={tweet.timestamp}/>      
+      </div>
 
-          <span className="timestamp">Nov 30, 2020</span>
-        </div>
+        <Message message={tweet.message}/>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment"></i>
-          <i className="fas fa-retweet"></i>
-          <i className="far fa-heart"></i>
-          <i className="fas fa-share"></i>
-        </div>
+        <Actions />
       </div>
 
       <i className="fas fa-ellipsis-h"></i>
