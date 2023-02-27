50a3b4624e5a9448b37728c97e63727438e4728d
Bernardo Vera <bvera.ruiz@gmail.com>
2023-02-27 16:05:09 +0100

    done

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,34 +1,29 @@
-function Tweet() {
+import Actions from "./Actions";
+import Message from "./Message";
+import ProfileImage from "./ProfileImage";
+import TimeStamp from "./Timestamp";
+import User from "./User";
+
+
+function Tweet({ tweet }) {
+
+  const { user, timestamp, message } = tweet
   return (
     <div className="tweet">
-      <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
-        className="profile"
-        alt="profile"
-      />
+
+      <ProfileImage image={user.image} />
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
 
-          <span className="timestamp">Nov 30, 2020</span>
+          <User userData={user} />
+
+          <TimeStamp time={timestamp} />
         </div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
+        <Message message={message} />
 
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment"></i>
-          <i className="fas fa-retweet"></i>
-          <i className="far fa-heart"></i>
-          <i className="fas fa-share"></i>
-        </div>
+        <Actions />
       </div>
 
       <i className="fas fa-ellipsis-h"></i>
---
+++ src/components/User.js
@@ -0,0 +1,14 @@
+function User({ userData }) {
+
+    const { name, handle } = userData
+
+    return (
+        <span className="user">
+            <span className="name">{name}</span>
+            <span className="handle">{handle}</span>
+        </span>
+    )
+
+}
+
+export default User
