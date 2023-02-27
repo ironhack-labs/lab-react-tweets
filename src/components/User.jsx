dbd607c6cf01bcc583d84c2168aac769410b9a48
Daniel Yanez <ingdanielyanez@gmail.com>
2023-02-27 16:50:47 +0100

    [PP DE 0123] ALDI - DANIEL YANEZ

---
+++ src/components/User.jsx
@@ -0,0 +1,11 @@
+const User = ({ userData }) => {
+    const { name, handle } = userData
+    return (
+        < span className="user" >
+            <span className="name">{name}</span>
+            < span className="handle" > {handle}</span >
+        </span >
+    )
+}
+
+export default User
