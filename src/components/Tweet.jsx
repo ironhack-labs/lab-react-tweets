7a970f4aef43957c99f5bd4c33ea4484ed1f4daa
Alexandre Alvaro <a.borges.alvaro@gmail.com>
2023-02-27 14:32:06 +0000

    finished lab

---
+++ src/components/Tweet.jsx
@@ -0,0 +1,33 @@
+import ProfileImage from './ProfileImage';
+import User from '../User';
+import Timestamp from './Timestamp';
+import Message from './Message';
+import Actions from './Actions';
+
+function Tweet(props) {
+  console.log(props);
+  const {tweet} = props
+  return (
+    <div className="tweet">
+      <ProfileImage image={tweet.user.image} />
+
+      <div className="body">
+        <div className="top">
+
+        <User userData={tweet.user} />
+
+        <Timestamp time={tweet.timestamp} />
+
+        </div>
+
+        <Message message={tweet.message} />
+
+        <Actions />
+      </div>
+
+      <i className="fas fa-ellipsis-h"></i>
+    </div>
+  );
+}
+
+export default Tweet;
