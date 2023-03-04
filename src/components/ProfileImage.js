0ea8d407ce82ed200785d908ea139d2c9564934f
lucas.padueli <lucascapellapadueli@gmail.com>
2023-03-03 22:36:18 -0300

    solved lab

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,17 @@
+function ProfileImage (props) {
+const {image} = props;
+    return (
+
+        <img
+        src= {image}
+        className="profile"
+        alt="profile"
+      />
+
+    )
+}
+
+export default ProfileImage;
+
+
+
