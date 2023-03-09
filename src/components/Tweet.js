75fff52643783fcee29fe7689139581fda553b1b
Dantas Beyer <dantasbeyer@gotmail.com>
2023-03-09 12:21:11 +0100

    interaction 7 done

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ProfileImage from './ProfileImage'
 import User from './User'
 import Timestamp from "./Timestamp"
+import Message from './Message'
 
 function Tweet(props) {
   return (
@@ -12,9 +13,8 @@ function Tweet(props) {
         <div className='top'>
           <User name={props.tweet.user.name}/>
           <Timestamp time={props.tweet.timestamp}/>
-
-      
         </div>
+        <Message message={props.tweet.message}/>
       </div>
     </div>
   )
