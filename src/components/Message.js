16471a3cda380912a9148d5e33f105b099c853e8
ficapp2.0 <ficapp2.0@gmail.com>
2023-03-09 22:27:38 +0100

    feat: final version

---
+++ src/components/Message.js
@@ -0,0 +1,9 @@
+function Message({ children }) {
+  return (
+    <p className="message">
+      { children }
+    </p>
+  )
+}
+
+export default Message
---
+++ src/components/ProfileImage.js
@@ -0,0 +1,12 @@
+
+function ProfileImage({ image }) {
+  return (
+    <img
+      src={ image }
+      className="profile"
+      alt="profile"
+    />
+  )
+}
+
+export default ProfileImage
