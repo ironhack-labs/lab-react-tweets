4d2c6caaa7ba00e7d6f080e8334cad0f92f7889e
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-03-09 23:19:41 +0100

    Iteration 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,11 @@
+import { tweetsArray } from "./api/tweet";
 import "./App.css";
 import Tweet from "./components/Tweet";
-
-const tweetsArray = [
-  {
-    user: {
-      name: "Thoughts of Dog®",
-      image: "https://i.imgur.com/b0EdHVV.jpg",
-      handle: "dog_feelings",
-    },
-    timestamp: "1h ago",
-    message:
-      "the human likes to say. that i live here rent free. but i would argue. this housing accommodation. is my payment. for a lifetime of love. and excellent company",
-  },
-  {
-    user: {
-      name: "Thoughts of Dog®",
-      image: "https://i.imgur.com/b0EdHVV.jpg",
-      handle: "dog_feelings",
-    },
-    timestamp: "2h ago",
-    message:
-      "sometimes. the human presses their noggin against mine. to figure out what i’m thinking. so i just think really hard. about how much i love them. and hope they figure it out",
-  },
-  {
-    user: {
-      name: "Thoughts of Dog®",
-      image: "https://i.imgur.com/b0EdHVV.jpg",
-      handle: "dog_feelings",
-    },
-    timestamp: "3h ago",
-    message:
-      "here is what. i plan to accomplish today: \n\n2. bark loudly. but at nothing \n7. lose my ball under the couch\n7b. politely ask the human. to get my ball\n3. immediately lose it again. under the same couch\n4. big nap. you have worked hard\n2. repeat",
-  },
-];
 
 const App = () => {
   return (
     <div className="App">
-      <Tweet />
+      <Tweet tweet={tweetsArray[0]} />
     </div>
   );
 };
