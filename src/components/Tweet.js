9b0a8495ccafa413c1da058bd533d1d4e84fef5c
FeliOjedaRuiz <feliojedaruiz@gmail.com>
2023-03-09 21:47:11 +0100

    Iteration 2 done

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,8 +1,8 @@
-function Tweet() {
+function Tweet({tweet}) {
   return (
     <div className="tweet">
       <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
+        src={tweet.user.image}
         className="profile"
         alt="profile"
       />
@@ -10,16 +10,15 @@ function Tweet() {
       <div className="body">
         <div className="top">
           <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
+            <span className="name">{tweet.user.name}</span>
+            <span className="handle">{tweet.user.handle}</span>
           </span>
 
-          <span className="timestamp">Nov 30, 2020</span>
+          <span className="timestamp">{tweet.timestamp}</span>
         </div>
 
         <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
+          {tweet.message}
         </p>
 
         <div className="actions">
