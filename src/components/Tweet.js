9d532a9a548b8bfd0ad4f4c74633b921e4503684
Dantas Beyer <dantasbeyer@gotmail.com>
2023-03-09 12:27:07 +0100

    done

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -3,6 +3,7 @@ import ProfileImage from './ProfileImage'
 import User from './User'
 import Timestamp from "./Timestamp"
 import Message from './Message'
+import Actions from './Actions'
 
 function Tweet(props) {
   return (
@@ -10,14 +11,15 @@ function Tweet(props) {
       <ProfileImage image={props.tweet.user.image}/>
 
       <div className='body'>
-        <div className='top'>
-          <User name={props.tweet.user.name}/>
-          <Timestamp time={props.tweet.timestamp}/>
-        </div>
+          <div className='top'>
+            <User name={props.tweet.user.name}/>
+            <Timestamp time={props.tweet.timestamp}/>
+          </div>
         <Message message={props.tweet.message}/>
+        <Actions/>
       </div>
     </div>
   )
 }
 
-export default Tweet
+export default Tweet;
