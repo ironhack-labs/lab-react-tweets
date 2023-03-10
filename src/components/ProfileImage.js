6c0f3e9926b0a875fd0b053da5ee51df4271aa8c
Victor Lopez <victorlpz2301@gmail.com>
2023-03-10 09:42:41 +0100

    iteration 3 & 4 completed

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,17 @@
+function ProfileImage ({ image }) {
+  return (
+  <img
+        src={image}
+        className="profile"
+        alt="profile"
+      />
+  )
+}
+
+
+ProfileImage.defaultProps = {
+  image: ''
+}
+
+
+export default ProfileImage
