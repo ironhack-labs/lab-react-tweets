ab23553b84da3e5a2e59e34e5528286a033ebf45
David Reguart <reguartdavid@gmail.com>
2023-03-10 21:37:50 +0100

    Lab Solved - React Tweets

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,39 +1,36 @@
-function Tweet() {
+import Actions from "./Actions";
+import Message from "./Message";
+import ProfileImage from "./ProfileImage";
+import Timestamp from "./Timestamp";
+import User from "./User";
+
+function Tweet({ tweet }) {
   return (
     <div className="tweet">
-      <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
-        className="profile"
-        alt="profile"
-      />
+      <ProfileImage image={tweet.user.image} />
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
-
-          <span className="timestamp">Nov 30, 2020</span>
+          <User name={tweet.user.name} handle={tweet.user.handle} />
+          <Timestamp timestamp={tweet.timestamp} />
         </div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
+        <Message message={tweet.message} />
 
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment"></i>
-          <i className="fas fa-retweet"></i>
-          <i className="far fa-heart"></i>
-          <i className="fas fa-share"></i>
-        </div>
+        <Actions />
       </div>
 
       <i className="fas fa-ellipsis-h"></i>
     </div>
   );
+}
+
+Tweet.defaultProps = {
+  name: '',
+  image: '',
+  handle: '',
+  timestamp: '',
+  message: ''
 }
 
 export default Tweet;
