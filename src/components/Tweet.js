2e6e07c3de92f94be362c6243f7c8b8fb2c40a63
Victor Lopez <victorlpz2301@gmail.com>
2023-03-10 10:27:12 +0100

    Iteration 6 completed

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,4 +1,5 @@
 import ProfileImage from "./ProfileImage";
+import Timestamp from "./Timestamp";
 import User from "./User";
 
 function Tweet(props) {
@@ -8,7 +9,7 @@ function Tweet(props) {
       <div className="body">
         <div className="top">
           <User userData={props.tweet.user} />
-          <span className="timestamp">{props.tweet.timestamp}</span>
+          <Timestamp time={props.tweet.timestamp} />
         </div>
 
         <p className="message">
