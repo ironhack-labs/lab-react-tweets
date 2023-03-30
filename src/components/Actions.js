e2a0f6e2f23995120c4c2fee8b42551210dd9092
YinongLi <yinong.li97@gmail.com>
2023-03-30 20:27:48 +0100

    solved lab

---
+++ src/components/Actions.js
@@ -0,0 +1,14 @@
+function Action(){
+    return (
+    <div className="actions">
+        {/* Font Awesome icons */}
+        <i className="far fa-comment"></i>
+        <i className="fas fa-reprops"></i>
+        <i className="far fa-heart"></i>
+        <i className="fas fa-share"></i>
+    </div>
+
+
+    )
+}
+export default Action;
