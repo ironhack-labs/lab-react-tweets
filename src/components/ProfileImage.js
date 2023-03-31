e5d067b9adcd0c8fe7a9bbcf402aec10147ea7ab
LKTOKB <theodiamanttt@gmail.com>
2023-03-31 16:50:48 +0200

    Lab Done

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,8 @@
+function ProfileImage(props){
+    const { image } = props
+    return (
+        <img src={image} className="profile" alt="profile"/>
+    )
+}
+
+export default ProfileImage
---
+++ src/components/Timestamp.js
@@ -0,0 +1,8 @@
+function TimeStamp(props) {
+    const { time } = props
+        return (
+            <span className="timestamp"> {time} </span>
+        )
+}
+
+export default TimeStamp
