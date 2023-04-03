f3f56f11447a9e663ddf7cf3ed792f3352a57656
Cristian Palao <cristianpalaocamarasa@gmail.com>
2023-04-03 17:00:37 +0200

    iteration 4

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,9 @@
+export const ProfileImage = (props) => {
+    return(
+        <img
+        src={props.image}
+        className="profile"
+        alt="profile"
+      />
+    );
+}
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,12 +1,11 @@
+import { ProfileImage } from "./ProfileImage";
+
 function Tweet(props) {
   return (
     <div className="tweet">
-      <img
-        src={props.tweet.user.image}
-        className="profile"
-        alt="profile"
-      />
 
+      <ProfileImage image={props.tweet.user.image} />
+      
       <div className="body">
         <div className="top">
           <span className="user">
