f7ca166fab43869ccaa2ae5282c5a34bf6884cdd
Peng Chen <chenpeng1221@outlook.com>
2023-04-03 20:15:21 +0200

    done

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,35 +1,26 @@
-function Tweet() {
+import ProfileImage from "./ProfileImage";
+import User from "./User";
+import Timestamp from "./Timestamp";
+import Message from "./Message";
+import Actions from "./Actions";
+
+
+function Tweet(props) {
+
   return (
     <div className="tweet">
-      <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
-        className="profile"
-        alt="profile"
-      />
+      <ProfileImage image={props.tweet.user.image} />
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
-
-          <span className="timestamp">Nov 30, 2020</span>
+          <User user={props.tweet.user}></User>
+          <Timestamp time={props.tweet.user.timestamp} ></Timestamp>
         </div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
+        <Message message={props.tweet.message}></Message>
+        <Actions></Actions>
 
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment"></i>
-          <i className="fas fa-retweet"></i>
-          <i className="far fa-heart"></i>
-          <i className="fas fa-share"></i>
-        </div>
-      </div>
+      </div >
 
       <i className="fas fa-ellipsis-h"></i>
     </div>
---
+++ src/components/User.js
@@ -0,0 +1,9 @@
+
+export default function User(props) {
+    return (
+        <span className="user">
+            <span className="name">{props.user.name}</span>
+            <span className="handle">@{props.user.handle}</span>
+        </span>
+    )
+}
