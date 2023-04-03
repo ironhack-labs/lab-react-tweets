e7641a1092e6afd3b4f211225e0374d5101e6b9a
valyy151 <marin.valenta1@gmail.com>
2023-04-03 11:58:06 +0200

    Iteration 2 complete

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,39 +1,36 @@
-function Tweet() {
-  return (
-    <div className="tweet">
-      <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
-        className="profile"
-        alt="profile"
-      />
+function Tweet({ tweet }) {
+	return (
+		<div className='tweet'>
+			<img
+				src={tweet.user.image}
+				className='profile'
+				alt='profile'
+			/>
 
-      <div className="body">
-        <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
+			<div className='body'>
+				<div className='top'>
+					<span className='user'>
+						<span className='name'>{tweet.user.name}</span>
+						<span className='handle'>{tweet.user.handle}</span>
+					</span>
 
-          <span className="timestamp">Nov 30, 2020</span>
-        </div>
+					<span className='timestamp'>{tweet.timestamp}</span>
+				</div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
+				<p className='message'>{tweet.message}</p>
 
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment"></i>
-          <i className="fas fa-retweet"></i>
-          <i className="far fa-heart"></i>
-          <i className="fas fa-share"></i>
-        </div>
-      </div>
+				<div className='actions'>
+					{/* Font Awesome icons */}
+					<i className='far fa-comment'></i>
+					<i className='fas fa-retweet'></i>
+					<i className='far fa-heart'></i>
+					<i className='fas fa-share'></i>
+				</div>
+			</div>
 
-      <i className="fas fa-ellipsis-h"></i>
-    </div>
-  );
+			<i className='fas fa-ellipsis-h'></i>
+		</div>
+	)
 }
 
-export default Tweet;
+export default Tweet
