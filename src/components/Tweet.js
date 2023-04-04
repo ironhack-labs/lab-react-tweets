661b39d1143d2ccdbb8ce67a14c0a6b897770da0
Michael <mickmhtong@gmail.com>
2023-04-05 01:25:30 +0800

    build: Iteration 9

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,23 +4,27 @@ import ProfileImage from "./ProfileImage";
 import Timestamp from "./Timestamp";
 import User from "./User";
 
-function Tweet({ tweet }) {
+function Tweet({ tweets }) {
   return (
-    <div className="tweet">
-      <ProfileImage image={tweet.user.image} />
-      <div className="body">
-        <div className="top">
-          <User userData={tweet.user} />
-          <Timestamp time={tweet.timestamp} />
+    <>
+      {tweets.map((tweet, tweetIdx) => (
+        <div className="tweet" key={tweetIdx}>
+          <ProfileImage image={tweet.user.image} />
+          <div className="body">
+            <div className="top">
+              <User userData={tweet.user} />
+              <Timestamp time={tweet.timestamp} />
+            </div>
+
+            <Message message={tweet.message} />
+
+            <Actions />
+          </div>
+
+          <i className="fas fa-ellipsis-h"></i>
         </div>
-
-        <Message message={tweet.message} />
-
-        <Actions />
-      </div>
-
-      <i className="fas fa-ellipsis-h"></i>
-    </div>
+      ))}
+    </>
   );
 }
 
