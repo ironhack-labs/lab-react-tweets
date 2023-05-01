1c99fd5eb02a4b1de5679e1314e688fbae599870
Nikolas <nihe@posteo.de>
2023-05-01 15:05:31 +0200

    [ber-wd-ft-mar23] Nikolas

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,34 +1,44 @@
-function Tweet() {
+import ProfileImage from "./ProfileImage";
+import User from "./User";
+import Timestamp from "./Timestamp"
+import Message from "./Message"
+import Actions from "./Actions"
+
+function Tweet(props) {
   return (
     <div className="tweet">
-      <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
-        className="profile"
-        alt="profile"
-      />
+      <ProfileImage image={props.tweet.user.image} />
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
+{/*           <span className="user">
+            <span className="name">{props.tweet.user.name}</span>
+            <span className="handle">{props.tweet.user.handle}</span>
+          </span> */}
+          {/* wird jetzt ersetzt durch: */}
+          <User userData={props.tweet.user}/>
 
-          <span className="timestamp">Nov 30, 2020</span>
+          {/* <span className="timestamp">{props.tweet.timestamp}</span> */}
+          {/* wird jetzt ersetzt durch: */}
+          <Timestamp time={props.tweet.timestamp} />
         </div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
+{/*         <p className="message">
+         {props.tweet.message}
+        </p> */}
+        {/* wird jetzt ersetzt durch: */}
+        <Message message={props.tweet.message}/>
 
-        <div className="actions">
-          {/* Font Awesome icons */}
+
+{/*         <div className="actions">
+          Font Awesome icons 
           <i className="far fa-comment"></i>
           <i className="fas fa-retweet"></i>
           <i className="far fa-heart"></i>
           <i className="fas fa-share"></i>
-        </div>
+        </div> */}
+        {/* wird jetzt ersetzt durch: */}
+        <Actions />
       </div>
 
       <i className="fas fa-ellipsis-h"></i>
