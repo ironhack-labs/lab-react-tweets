4604bd17e66313f230ec88fddd44164d56129f9d
Margarida Pereira <map.margaridapereira@gmail.com>
2023-05-22 16:18:26 +0100

    done

---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+function User (props){
+    let {userData} = props;
+    return (
+        <span className="user">
+          <span className="name">{userData.name}</span>
+          <span className="handle">{userData.handle}</span>
+        </span>
+      )
+    }
+
+export default User;
