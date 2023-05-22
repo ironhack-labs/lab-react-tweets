a8ea8b4ed3863c2c71738bf21beddc19693701e8
Elnaz Fe <elnaz.farrokhi1988@gmail.com>
2023-05-22 16:07:25 +0100

    [LIS-WDFT-Apr23]Elnaz

---
+++ src/components/Message.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Message(props){
+    let {message} = props;
+    return(
+        <div>
+            <p className="message">{message}</p>
+        </div>
+    )
+}
+export default Message;
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,39 +1,29 @@
-function Tweet() {
-  return (
-    <div className="tweet">
-      <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
-        className="profile"
-        alt="profile"
-      />
+import React from 'react';
+import ProfileImage from './ProfileImage';
+import User from './User';
+import Timestamp from './Timestamp';
+import Message from './Message';
+import Action from './Action';
 
-      <div className="body">
-        <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
+function Tweet(props) {
+return (
+  <div className="tweet">
+    <ProfileImage image={props.tweet.user.image} />
 
-          <span className="timestamp">Nov 30, 2020</span>
-        </div>
-
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment"></i>
-          <i className="fas fa-retweet"></i>
-          <i className="far fa-heart"></i>
-          <i className="fas fa-share"></i>
-        </div>
+    <div className="body">
+      <div className="top">
+        <User userData={props.tweet.user} />
+        <Timestamp time={props.tweet.timestamp} />
       </div>
 
-      <i className="fas fa-ellipsis-h"></i>
+      <Message message={props.tweet.message} />
+      <Action/>
+      
     </div>
-  );
+
+    <i class="fas fa-ellipsis-h"></i>
+  </div>
+);
 }
 
 export default Tweet;
