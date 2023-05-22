5a8c4b7faac842fdd2a692e5c13dba00cc3bb412
Marisa-Pinheiro <marisa.nbsp@gmail.com>
2023-05-22 16:17:04 +0100

    lab done

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,9 @@
+function ProfileImage(image) {
+  return (
+    <div>
+      <img src={image.image} className="profile" alt="profile" />
+    </div>
+  );
+}
+
+export default ProfileImage;
---
+++ src/components/User.js
@@ -0,0 +1,10 @@
+function User(userData) {
+  return (
+    <span className="user">
+      <span className="name">{userData.userData.name}</span>
+      <span className="handle">{userData.userData.handle}</span>
+    </span>
+  );
+}
+
+export default User;
