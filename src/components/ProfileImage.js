88ee97692a741048738fcb21e9f2a611baa2ab21
Niroj167 <gniroj964@gmail.com>
2023-05-23 07:13:37 +0100

    lab done

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,12 @@
+function ProfileImage(props){
+    let {image} = props
+    return(
+        <div>
+         <img src={image} className="profile" alt="profile"/>
+        </div>
+       
+    );
+
+}
+
+export default ProfileImage;
