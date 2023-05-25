187fb07b2f8c0a7fcec6e11cbe0096d419f7350b
pepealacid <ppalacid@gmail.com>
2023-05-25 13:34:10 +0200

    no actions

---
+++ src/components/ProfileImage.jsx
@@ -0,0 +1,11 @@
+function profileImage(props) {
+    return (
+        <img
+        src={props.image}
+        className="profile"
+        alt="profile"
+      />
+    )
+}
+
+export default profileImage
