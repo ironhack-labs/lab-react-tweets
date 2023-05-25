319da11b0ef2809b0ca00a8314bde35fdd83a267
Lena Imdahl <mail@lenaimdahl.de>
2023-05-25 21:29:47 +0200

    started iteration 5

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,4 +1,5 @@
 import ProfileImage from "./ProfileImage";
+import User from "./User";
 
 function Tweet({ tweet }) {
   return (
@@ -7,10 +8,7 @@ function Tweet({ tweet }) {
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">{tweet.user.name}</span>
-            <span className="handle">{tweet.user.handle}</span>
-          </span>
+          <User userData={tweet.user} />
 
           <span className="timestamp">{tweet.timestamp}</span>
         </div>
