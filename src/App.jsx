73b05f4a5b86526bf4b4904512baacf683cb1405
Lena Imdahl <mail@lenaimdahl.de>
2023-05-26 08:33:02 +0200

    Iteration 9

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,13 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet tweet={tweetsArray[0]} />
+      {tweetsArray.map((eachTweet) => {
+        return (
+          <div key={eachTweet.timestamp}>
+            <Tweet tweet={eachTweet} />
+          </div>
+        );
+      })}
     </div>
   );
 }
