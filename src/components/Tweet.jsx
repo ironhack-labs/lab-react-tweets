8de582ee596d7349043267085cfec91035fe7d96
kobetoto <106164290+kobetoto@users.noreply.github.com>
2023-06-10 11:45:40 +0200

    finish

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,40 +1,24 @@
-
-
+import ProfileImage from "./ProfileImage";
+import User from "./User";
+import Timestamp from "./Timestamp";
+import Message from "./Message";
+import Actions from "./Actions";
 
 function Tweet(props) {
- console.log("props====", props)
+  console.log("props====", props.tweet);
   return (
     <div className="tweet">
-      <img
-        src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/ih_logo.jpeg"
-        className="profile"
-        alt="profile"
-      />
-
+      <ProfileImage image={props.tweet.user.image} />
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">{props.tweet.user.name}</span>
-            <span className="handle">@ironhack</span>
-          </span>
-
-          <span className="timestamp">{props.tweet.timestamp}</span>
+          <User name={props.tweet.user.name} handle={props.tweet.user.handle} />
+          <Timestamp time={props.tweet.timestamp} />
         </div>
-
-        <p className="message">
-        {props.tweet.message}
-        </p>
-
+        <Message message={props.tweet.message} />
         <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment"></i>
-          <i className="fas fa-retweet"></i>
-          <i className="far fa-heart"></i>
-          <i className="fas fa-share"></i>
+          <Actions />
         </div>
       </div>
-
-      <i className="fas fa-ellipsis-h"></i>
     </div>
   );
 }
