d326a81d84a623e922d638c1feec891a34a26da3
Alex Vinogradov <yarmeg76@gmail.com>
2023-06-17 21:27:30 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import ProfileImage from "./components/ProfileImage";
 import Tweet from "./components/Tweet";
 
 const tweetsArray = [
@@ -37,7 +38,9 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet />
+      <Tweet tweet={tweetsArray[0]} />
+      <Tweet tweet={tweetsArray[1]} />
+      <Tweet tweet={tweetsArray[2]} />
     </div>
   );
 }
