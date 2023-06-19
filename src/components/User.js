5c4f80095fdafd130dd89db143387c0edfbd0961
Emily Stevenson <e.stevenson6@hotmail.co.uk>
2023-06-19 16:47:29 +0100

    done

---
+++ src/components/User.js
@@ -0,0 +1,9 @@
+function User(props){
+    return(
+<span className="user">
+  <span className="name">{props.user.name}</span>
+<span className="handle">{props.user.handle}</span>
+</span>
+    )}
+
+export default User
