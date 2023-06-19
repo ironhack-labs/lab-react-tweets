41511a219f1827435e11e3a1abd4e1e4df4c16b0
Solideo Putra Zendrato <solidzendrato@gmail.com>
2023-06-19 18:07:27 +0200

    done

---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+function User(props) {
+  console.log(props);
+  return(
+    <>
+      <span className="name">{props.user.name}</span>
+      <span className="handle">{props.user.handle}</span>
+    </> 
+  )
+}
+
+export default User;
