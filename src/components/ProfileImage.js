ba94ae9602551d3f6a458f9bfedf353910787d98
nunojsmonteiro <nunojsmonteiro@gmail.com>
2023-06-19 18:06:58 +0100

    lab done

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,10 @@
+function ProfileImage(props) {
+    console.log(props)
+    return (
+        <img src={props.image} className="profile" alt="profile" 
+        />
+    );
+  }
+
+  
+export default ProfileImage;
