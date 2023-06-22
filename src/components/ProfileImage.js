7324f8e0585613eefc54540558c8969c5539e677
Nishtha Agarwal <nishtha.agarwal26@gmail.com>
2023-06-22 09:54:27 +0200

    done

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,12 @@
+//import "./App.css";
+function ProfileImage() {
+return(
+   
+ <img
+   src="https://i.imgur.com/9yw1Fyw.jpg"
+   className="profile"
+   alt="profile"
+ />
+);
+}
+export default ProfileImage;
---
+++ src/components/User.js
@@ -0,0 +1,13 @@
+//import "./App.css";
+function User(props) {
+return(
+
+//<div className="body">
+//<div className="top">
+  <span className="user">
+    <span className="name">{props.tweet.user}</span>
+    <span className="handle">@ironhack</span>
+  </span>
+  );
+}
+  export default User;
