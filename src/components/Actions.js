8d76400d8df1b743d0a15f04bb1697d177e0ef77
Jonathan Bijl <sannoayo@gmail.com>
2023-07-01 08:20:37 -0400

    Iternation 9-Incomplete

--- src/components/Actions.js
+++ src/components/Actions.js
@@ -1,13 +1,12 @@
 function Actions() {
 	return (
-        <div className="actions">
-            <i class="far fa-comment"></i>
-            <i class="fas fa-retweet"></i>
-            <i class="far fa-heart"></i>
-             <i class="fas fa-share"></i>
-        </div>
-        
-
+		<div className="actions">
+			{/* Font Awesome icons */}
+			<i className="far fa-comment"></i>
+			<i className="fas fa-retweet"></i>
+			<i className="far fa-heart"></i>
+			<i className="fas fa-share"></i>
+		</div>
 	);
 }
 
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,8 +1,9 @@
-import ProfileImage from "./ProfileImage";
-import User from "./User";
-import Timestamp from "./Timestamp";
-import Message from "./Message";
-import Actions from "./Actions";
+import React from 'react';
+import ProfileImage from './ProfileImage';
+import UserComponent from './User';
+import TimestampComponent from './Timestamp';
+import Message from './Message';
+import Actions from './Actions';
 
 function Tweet(props) {
   return (
@@ -10,20 +11,18 @@ function Tweet(props) {
       <ProfileImage image={props.tweet.user.image} />
 
       <div className="body">
-        
         <div className="top">
-          <User userData={props.tweet.user} />
-          <Timestamp time={props.tweet.timestamp} />
+          <UserComponent userData={props.tweet.user} />
+          <TimestampComponent time={props.tweet.timestamp} />
         </div>
-
         <Message message={props.tweet.message} />
         <Actions />
-        
       </div>
 
-      <i class="fas fa-ellipsis-h"></i>
+      <i className="fas fa-ellipsis-h"></i>
     </div>
   );
 }
 
 export default Tweet;
+   
