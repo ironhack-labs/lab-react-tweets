8afff93602541770c524e27cf675479331a49213
Aykut Kav <ayktkav@gmail.com>
2023-07-10 19:58:47 +0200

    Solved lab

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,34 +1,38 @@
-function Tweet() {
+import Actions from "./Actions";
+import Message from "./Message";
+import Timestamp from "./Timestamp";
+import User from "./User";
+import ProfileImage from "./ProfileImage";
+
+function Tweet(props) {
+  const { name, image, handle } = props.tweet.user;
+  const { timestamp, message } = props.tweet;
+
   return (
     <div className="tweet">
-      <img
-        src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/ih_logo.jpeg"
-        className="profile"
-        alt="profile"
-      />
+      <ProfileImage image={image} />
+      {/* <img src={image} className="profile" alt="profile" /> */}
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
-
-          <span className="timestamp">Nov 30, 2020</span>
+          <User name={name} handle={handle} />
+          {/* <span className="user">
+            <span className="name">{name}</span>
+            <span className="handle">{handle}</span>
+          </span> */}
+          <Timestamp timestamp={timestamp} />
+          {/* <span className="timestamp">{timestamp}</span> */}
         </div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
-
-        <div className="actions">
-          {/* Font Awesome icons */}
+        <Message message={message} />
+        {/* <p className="message">{message}</p> */}
+        <Actions></Actions>
+        {/* <div className="actions">
           <i className="far fa-comment"></i>
           <i className="fas fa-retweet"></i>
           <i className="far fa-heart"></i>
           <i className="fas fa-share"></i>
-        </div>
+        </div> */}
       </div>
 
       <i className="fas fa-ellipsis-h"></i>
