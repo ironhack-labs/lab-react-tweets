9d73e14a5588d8555c2ef854601213e824c05ed8
Mirza Abdullah Baig <abdullah.baig2013@gmail.com>
2023-07-11 10:01:21 +0200

    Done

---
+++ src/components/Timestamp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Timestamp({ time }) {
+  return <span className="timestamp">{time}</span>;
+}
+
+export default Timestamp;
--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,34 +1,40 @@
-function Tweet() {
+import ProfileImage from "./ProfileImage";
+import User from "./User";
+import Timestamp from "./Timestamp";
+import Message from "./Message";
+import Actions from "./Actions";
+
+function Tweet({ tweet }) {
   return (
     <div className="tweet">
-      <img
-        src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/ih_logo.jpeg"
-        className="profile"
-        alt="profile"
-      />
+      <ProfileImage image={tweet.user.image} />
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
+          <User name={tweet.user.name} handle={tweet.user.handle} />
+          <Timestamp time={tweet.timestamp} />
+          <div>
+            <Message message={tweet.message} />
+            <Actions />
+          </div>
+          {/* <span className="user">
+            <span className="name">{tweet.user.name}</span>
+            <span className="handle">{tweet.user.handle}</span>
+          </span> */}
 
-          <span className="timestamp">Nov 30, 2020</span>
+          {/* <span className="timestamp">{tweet.timestamp}</span> */}
         </div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
+        {/* <p className="message">{tweet.message}</p> */}
 
-        <div className="actions">
-          {/* Font Awesome icons */}
+        {/* 
+          <div className="actions">
           <i className="far fa-comment"></i>
           <i className="fas fa-retweet"></i>
           <i className="far fa-heart"></i>
           <i className="fas fa-share"></i>
         </div>
+        */}
       </div>
 
       <i className="fas fa-ellipsis-h"></i>
