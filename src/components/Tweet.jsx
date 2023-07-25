6e4d5db5d760a054ae5d1dbfbbfbbdd45a62ef56
Katia <kate.liashevska@gmail.com>
2023-07-25 17:29:47 +0200

    lab done but one test is broken

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,39 +1,28 @@
-function Tweet() {
-  return (
-    <div className="tweet">
-      <img
-        src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/ih_logo.jpeg"
-        className="profile"
-        alt="profile"
-      />
+import ProfileImage from "./ProfileImage"
+import User from "./User"
+import Timestamp from "./Timestamp"
+import Message from "./Message"
+import Actions from "./Actions"
 
-      <div className="body">
-        <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
+function Tweet(props) {
+    return (
+        <div className="tweet">
+            <ProfileImage image={props.tweet.user.image} />
 
-          <span className="timestamp">Nov 30, 2020</span>
+            <div className="body">
+                <div className="top">
+                    <User name={props.tweet.user.name} handle={props.tweet.user.handle} />
+                    <Timestamp time={props.tweet.timestamp} />
+                </div>
+
+                <Message message={props.tweet.message} />
+
+                <Actions />
+            </div>
+
+            <i className="fas fa-ellipsis-h"></i>
         </div>
-
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment"></i>
-          <i className="fas fa-retweet"></i>
-          <i className="far fa-heart"></i>
-          <i className="fas fa-share"></i>
-        </div>
-      </div>
-
-      <i className="fas fa-ellipsis-h"></i>
-    </div>
-  );
+    )
 }
 
-export default Tweet;
+export default Tweet
