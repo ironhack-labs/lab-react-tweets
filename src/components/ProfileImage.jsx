04a1a1b8bff2f910540bfafdeacc9e04ab432433
Luis <luisguile495@gmail.com>
2023-07-28 00:13:58 -0400

    Lab done

---
+++ src/components/ProfileImage.jsx
@@ -0,0 +1,7 @@
+const ProfileImage = (props) => {
+    return(
+        <img src={props.image} alt="Profile" className="profile" />
+    )
+}
+
+export default ProfileImage;
---
+++ src/components/TimeStamp.jsx
@@ -0,0 +1,9 @@
+const TimeStamp = (props) => {
+    return(
+        <span className="timestamp">
+            {props.time}
+        </span>
+    )
+}
+
+export default TimeStamp;
