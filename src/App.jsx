0e152e6bafb95955045c9753364a4513680775dc
tiagomgrodrigues <tiagomgr97@gmail.com>
2023-08-21 19:47:26 +0100

    lab done

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,16 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet />
+      {tweetsArray.map((tweet) => {
+        return (
+          <div>
+            <Tweet tweet={tweet} />
+          </div>
+        );
+      })}
+      {/* <Tweet tweet={tweetsArray[0]} />
+      <Tweet tweet={tweetsArray[1]} />
+      <Tweet tweet={tweetsArray[2]} /> */}
     </div>
   );
 }
---
+++ src/components/Timestamp.jsx
@@ -0,0 +1,9 @@
+function Timestamp(props) {
+  return (
+    <div>
+      <span className="timestamp">{props.time_timestamp}</span>
+    </div>
+  );
+}
+
+export default Timestamp;
