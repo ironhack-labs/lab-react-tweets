b6c73919676ab38090e9c289b91a48c199fef867
ogberagabriel234@gmail.com <ogberagabriel234@gmail.com>
2023-08-21 16:12:29 +0100

    Lab-Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+
 import Tweet from "./components/Tweet";
 
 const tweetsArray = [
@@ -40,7 +41,9 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet />
+      <Tweet tweet={ tweetsArray[0] }/>
+      <Tweet tweet={ tweetsArray[1] }/>
+      <Tweet tweet={ tweetsArray[2] }/>
     </div>
   );
 }
