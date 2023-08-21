b90bb20d713435a1c47e52f634aeed04fcc9ed4b
Agnieszka Morawska <agnesmora.art@gmail.com>
2023-08-21 16:48:27 +0100

    lab completed

---
+++ src/components/ProfileImage.jsx
@@ -0,0 +1,12 @@
+function ProfileImage(props) {
+
+    return (
+    <div>
+        <img src="https://i.imgur.com/b0EdHVV.jpg"
+        className="profile"
+        alt="profile" />
+        </div>
+
+    )}
+
+    export default ProfileImage;
---
+++ src/components/Timestamp.jsx
@@ -0,0 +1,12 @@
+function Timestamp(props) {
+    return (
+        <div>
+        <span className="timestamp"> TWEET_TIMESTAMP 
+            <span className="time"> {props.time} </span>
+        </span>
+        </div>
+    );
+    }
+    
+    export default Timestamp;
+    
