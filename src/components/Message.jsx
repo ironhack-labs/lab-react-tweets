99d6af61556b0ced055c8534a95668b483c9e7b8
jsguillotte <julia.guillotte@gmail.com>
2023-08-21 10:48:17 -0500

    solved lab

---
+++ src/components/Message.jsx
@@ -0,0 +1,14 @@
+function Message(props) {
+    //const{tweet} = props
+    return (
+      <div>
+        
+
+        <p className="message"> {props.message}</p>
+
+         </div> 
+
+    )
+}
+
+export default Message
