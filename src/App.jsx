241cf1cd536f962bdd2b46729bcccf62d27347d5
Manuel Segarra <manuel.gs.segarra@gmail.com>
2023-08-21 16:34:52 +0100

    solved lab

--- src/App.jsx
+++ src/App.jsx
@@ -40,8 +40,11 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet />
+      {tweetsArray.map((tweet)=>{
+        return(<Tweet tweet={tweet}/>)
+      })}
     </div>
+
   );
 }
 
