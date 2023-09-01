21d2d730cc134f725832e10f49cd8671b456c712
Juan Diego Sanchez <diegosanchezperez93@gmail.com>
2023-09-01 20:09:23 +0200

    Iteration 9

--- src/App.jsx
+++ src/App.jsx
@@ -36,13 +36,19 @@ const tweetsArray = [
       "here is what. i plan to accomplish today: \n\n2. bark loudly. but at nothing \n7. lose my ball under the couch\n7b. politely ask the human. to get my ball\n3. immediately lose it again. under the same couch\n4. big nap. you have worked hard\n2. repeat",
   },
 ];
-
 function App() {
+
   return (
-    <div className="App">
-      <Tweet tweet={ tweetsArray[0] }/>
-    </div>
+    <>
+      {tweetsArray.map((tweet, index) => (
+        <Tweet
+          tweet={tweet}
+          key={index}
+        />
+      ))}
+    </>
   );
+  
 }
 
 export default App;
