ad9f0d9a131d2460d262ba2527e2be71fcfdc320
Juan Cruz <j.cruzb@outlook.com>
2023-09-03 16:12:57 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,18 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet />
+      {
+        tweetsArray.map((tweet, index) => {
+          return (
+            <Tweet 
+              key={index}
+              user={tweet.user}
+              timestamp={tweet.timestamp}
+              message={tweet.message}
+            />
+          )
+        })
+      }
     </div>
   );
 }
