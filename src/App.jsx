270881a5bd69fe397017cc6e0baa384d29acc13a
Anna Kazulak <anna.kazulak@gmail.com>
2023-09-11 18:09:42 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,13 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet />
+      {/* <Tweet tweet={tweetsArray[0]} />
+      <Tweet tweet={tweetsArray[1]} />
+      <Tweet tweet={tweetsArray[2]} /> */}
+
+     { tweetsArray.map((tweet) =>{
+      return  <Tweet key={tweet.message} tweet={tweet} />
+      })}
     </div>
   );
 }
---
+++ src/components/Actions.jsx
@@ -0,0 +1,12 @@
+function Actions() {
+  return (
+    <div className="actions">
+      <i className="far fa-comment" data-testid="comment-icon"></i>
+      <i className="fas fa-retweet" data-testid="retweet-icon"></i>
+      <i className="far fa-heart" data-testid="heart-icon"></i>
+      <i className="fas fa-share" data-testid="share-icon"></i>
+    </div>
+  );
+}
+
+export default Actions;
