4730c88653f1346e903df9755b7defde8063c72f
Beatriz <beatrizrmb@outlook.com>
2023-09-11 19:45:20 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Tweet from "./components/Tweet";
 
+
 const tweetsArray = [
   {
     user: {
@@ -40,7 +41,12 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet />
+      {tweetsArray.map((tweet) =>(
+        <Tweet
+          key={tweet.user.handle + tweet.timestamp}
+          tweet={tweet} 
+        />
+      ))}
     </div>
   );
 }
---
+++ src/components/Timestamp.jsx
@@ -0,0 +1,7 @@
+export default function Timestamp({time}) {
+    return (
+    <>
+        <span className="timestamp">{time}</span>
+    </>
+    )
+}
