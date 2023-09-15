a9a8dec452d7031129d45af73ded27417e9d6538
Cyclonej <cyclonejmoto@gmail.com>
2023-09-15 09:36:53 +0200

    change some css text size and boldness

--- src/App.jsx
+++ src/App.jsx
@@ -40,9 +40,10 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet tweet={ tweetsArray[0] }/>
-      <Tweet tweet={ tweetsArray[1] }/>
-      <Tweet tweet={ tweetsArray[2] }/>
+      {/* <Tweet tweet={tweetsArray[0]} /> */}
+      {tweetsArray.map((eachTweet, i) => {
+        return <Tweet tweet={eachTweet} key={i} />;
+      })}
     </div>
   );
 }
--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -10,30 +10,21 @@ function Tweet(props) {
   return (
     <div className="tweet">
 
-      <ProfileImage image={props.tweet.user.image}
-
-      //   src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/ih_logo.jpeg"
-      //   className="profile"
-      //   alt="profile"
-      />
-
+      <ProfileImage image={props.tweet.user.image} />
       <div className="body">
+
         <div className="top">
           <User name={props.tweet.user.name} handle={props.tweet.user.handle} />
-
           <Timestamp time={props.tweet.timestamp} />
         </div>
 
         <p className="message">
           <Message message={props.tweet.message} />
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
         </p>
-
 
         <Actions />
       </div>
-      
+
       <i className="fas fa-ellipsis-h"></i>
     </div>
   );
