9984faebeeb448b4b2e6b3a55a2d484d6de4caa0
João Valente <joao.valente5935@gmail.com>
2023-10-31 18:52:24 +0000

    done

---
+++ src/components/User.jsx
@@ -0,0 +1,12 @@
+export default function User(props){
+    return(
+        <>
+            <span className="user">
+                <span className="name">{props.name}</span>
+                <span className="handle">@{props.handle}</span>
+            </span>
+        </>
+
+    )
+
+}
