bdf8e10f53a391c871b5d1c8c96c8ee069b1a349
Nqbral <sim.chevalier@gmail.com>
2023-11-01 19:55:45 +0100

    done

--- src/test/User.test.jsx
+++ src/test/User.test.jsx
@@ -3,20 +3,18 @@ import { describe, expect, test } from "vitest";
 import User from "../components/User";
 
 describe("User component", () => {
-  const tweet = {
-    user: {
-      name: "Thoughts of Dog®",
-      image: "https://i.imgur.com/b0EdHVV.jpg",
-      handle: "dog_feelings",
-    },
-    timestamp: "1h ago",
-    message: "the human likes to say. that i live here rent free.",
-  };
+  // const tweet = {
+  //   user: {
+  //     name: "Thoughts of Dog®",
+  //     image: "https://i.imgur.com/b0EdHVV.jpg",
+  //     handle: "dog_feelings",
+  //   },
+  //   timestamp: "1h ago",
+  //   message: "the human likes to say. that i live here rent free.",
+  // };
 
   test("renders user's name and handle correctly", () => {
-    render(
-      <User name={"Thoughts of Dog®"} handle={"dog_feelings"} />
-    );
+    render(<User name={"Thoughts of Dog®"} handle={"dog_feelings"} />);
     const nameText = screen.getByText("Thoughts of Dog®");
     const handleText = screen.getByText("@dog_feelings");
 
