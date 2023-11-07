f055fa5ac1d144dfeb79b0ec9d37a99f7425c6e6
Vidisha Shah <shahvidisha1996@gmail.com>
2023-11-07 16:42:09 +0100

    Lab done

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,34 +1,23 @@
-function Tweet() {
+import Actions from "./Actions";
+import Message from "./Message";
+import ProfileImage from "./ProfileImage";
+import Timestamp from "./Timestamp";
+import User from "./User";
+
+function Tweet(props) {
   return (
     <div className="tweet">
-      <img
-        src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/ih_logo.jpeg"
-        className="profile"
-        alt="profile"
-      />
+    <ProfileImage img = {props.tweet.user.image}/>
+    
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
-
-          <span className="timestamp">Nov 30, 2020</span>
+        <User name = {props.tweet.user.name} handle = {props.tweet.user.handle} />
+        <Timestamp timestamp={props.tweet.timestamp}/>  
         </div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment" data-testid="comment-icon"></i>
-          <i className="fas fa-retweet" data-testid="retweet-icon"></i>
-          <i className="far fa-heart" data-testid="heart-icon"></i>
-          <i className="fas fa-share" data-testid="share-icon"></i>
-        </div>
+        <Message message = {props.tweet.message}/>
+        <Actions/>
       </div>
 
       <i className="fas fa-ellipsis-h"></i>
---
+++ src/components/User.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function User(props) {
+  return (
+    <span className="user">
+      <span className="name">{props.name}</span>
+      <span className="handle">{props.handle}</span>
+    </span>
+  );
+}
+
+export default User;
