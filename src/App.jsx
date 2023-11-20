63deb49bed617c5cb48359fb6943d372746375e4
Dixamo <juandarm02@gmail.com>
2023-11-20 15:25:51 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet />
+      {tweetsArray.map((tweet, i) => <Tweet tweet={ tweet } key={ i }/>)}
     </div>
   );
 }
