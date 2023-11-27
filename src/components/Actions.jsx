7dc92cde359285cb62d640211873e0ca8d3559d3
Lou Gastardi <l.gastardi@hotmail.com>
2023-11-27 18:14:57 +0100

    lab Done

---
+++ src/components/Actions.jsx
@@ -0,0 +1,10 @@
+export default function Actions() {
+  return (
+    <div className="actions">
+      <i class="far fa-comment" data-testid="comment-icon"></i>
+      <i class="fas fa-retweet" data-testid="retweet-icon"></i>
+      <i class="far fa-heart" data-testid="heart-icon"></i>
+      <i class="fas fa-share" data-testid="share-icon"></i>
+    </div>
+  );
+}
---
+++ src/components/ProfileImage.jsx
@@ -0,0 +1,3 @@
+export default function ProfileImage({ profileImgSrc }) {
+  return <img src={profileImgSrc} className="profile" alt="profile" />;
+}
