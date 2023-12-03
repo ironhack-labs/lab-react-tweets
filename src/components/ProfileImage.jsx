35e3df9309939b6b0b29593cfac82c2109f7e71b
Pierre R Galette <pierre.galette@protonmail.com>
2023-12-03 16:26:19 -0500

    Completed Lab

---
+++ src/components/ProfileImage.jsx
@@ -0,0 +1,9 @@
+const ProfileImage = ({ image }) => {
+  return (
+    <>
+      <img src={image} className="profile" alt="profile" />
+    </>
+  );
+};
+
+export default ProfileImage;
--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,36 +1,24 @@
-function Tweet() {
+import ProfileImage from "./ProfileImage";
+import User from "./User";
+import Timestamp from "./Timestamp";
+import Message from "./Message";
+import Actions from "./Actions";
+function Tweet({ tweet }) {
+  const { user, timestamp, message } = tweet;
+
   return (
     <div className="tweet">
-      <img
-        src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/ih_logo.jpeg"
-        className="profile"
-        alt="profile"
-      />
+      <ProfileImage image={user.image} />
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
-
-          <span className="timestamp">Nov 30, 2020</span>
+          <User user={user} />
+          <Timestamp timestamp={timestamp} />
         </div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment" data-testid="comment-icon"></i>
-          <i className="fas fa-retweet" data-testid="retweet-icon"></i>
-          <i className="far fa-heart" data-testid="heart-icon"></i>
-          <i className="fas fa-share" data-testid="share-icon"></i>
-        </div>
+        <Message message={message} />
+        <Actions />
       </div>
-
       <i className="fas fa-ellipsis-h"></i>
     </div>
   );
