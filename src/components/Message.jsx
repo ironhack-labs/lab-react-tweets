3a168aa8ba0bcc8f8a8c4b4e55cd30e8e3bf9f8c
Gabriel Fernandez <gfernandezareas@gmail.com>
2024-01-15 21:21:31 +0100

    lab done

---
+++ src/components/Message.jsx
@@ -0,0 +1,11 @@
+const Message = props => {
+    return (
+        <div>
+            <p className="message">
+                {props.message}
+            </p>
+        </div>
+    );
+}
+
+export default Message;
