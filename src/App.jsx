03291b9b3ca4c6489009784c0214fd7e71801a06
MonicaAgrela <monicacosta_6@hotmail.com>
2024-01-19 20:39:36 +0000

    lab-react-tweets-vite Jorge&Mónica

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,8 @@ function App() {
   return (
     <div className="App">
       <Tweet tweet={tweetsArray[0]}></Tweet>
+      <Tweet tweet={tweetsArray[1]}></Tweet>
+      <Tweet tweet={tweetsArray[2]}></Tweet>
     </div>
   );
 }
---
+++ src/components/Timestamp.jsx
@@ -0,0 +1,6 @@
+function Timestamp(props){
+    return (
+        <span className="timestamp">{props.timestamp}</span>
+    )
+}
+export default Timestamp
