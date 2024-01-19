1e83ccab74a9ad341c14132e8d6a257641b9aac7
Christian Doeller <mail@christiandoeller.de>
2024-01-19 13:59:03 +0100

    finished iterations

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,36 +1,27 @@
-function Tweet() {
+import Actions from "./Actions";
+import Message from "./Message";
+import ProfileImage from "./ProfileImage";
+import Timestamp from "./Timestamp";
+import User from "./User";
+
+function Tweet(props) {
+  //console.log("tweetlog");
+  console.log(props.tweet.user.name);
+
   return (
     <div className="tweet">
-      <img
-        src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/ih_logo.jpeg"
-        className="profile"
-        alt="profile"
-      />
-
+      <ProfileImage image={props.tweet.user.image}></ProfileImage>
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
-
-          <span className="timestamp">Nov 30, 2020</span>
+          <User
+            userName={props.tweet.user.name}
+            userHandle={props.tweet.user.handle}
+          ></User>
+          <Timestamp timeStamp={props.tweet.timestamp}></Timestamp>
         </div>
-
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment" data-testid="comment-icon"></i>
-          <i className="fas fa-retweet" data-testid="retweet-icon"></i>
-          <i className="far fa-heart" data-testid="heart-icon"></i>
-          <i className="fas fa-share" data-testid="share-icon"></i>
-        </div>
+        <Message message={props.tweet.message}></Message>
+        <Actions></Actions>
       </div>
-
       <i className="fas fa-ellipsis-h"></i>
     </div>
   );
---
+++ src/components/User.jsx
@@ -0,0 +1,11 @@
+function User(props) {
+  return (
+    <>
+      <span className="user">
+        <span className="name">{props.userName}</span>
+        <span className="handle">{props.userHandle}</span>
+      </span>
+    </>
+  );
+}
+export default User;
