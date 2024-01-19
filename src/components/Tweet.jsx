059e94a3ec261f17a39416d403d9e4aa33e6ebfc
Natalia Pinto <pinto.natalia@outlook.com>
2024-01-19 17:01:50 +0100

    Lab completed

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -2,31 +2,28 @@
 import ProfileImage from "./ProfileImage";
 import User from "./User";
 import Timestamp from "./Timestamp";
+import Actions from "./Actions";
+import Message from "./Message";
 function Tweet(props) {
   return (
     <div className="tweet">
       <ProfileImage image={props.tweet.user.image}></ProfileImage>
-
       <div className="body">
         <div className="top">
-           <User>name={props.tweet.user.name}
-                handle=@{props.tweet.user.handle}
-                </User>
-          <Timestamp>time={props.tweet.timestamp}</Timestamp>
-        </div>
-
-        <p className="message">{props.tweet.message}
-        </p>
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment" data-testid="comment-icon"></i>
-          <i className="fas fa-retweet" data-testid="retweet-icon"></i>
-          <i className="far fa-heart" data-testid="heart-icon"></i>
-          <i className="fas fa-share" data-testid="share-icon"></i>
-        </div>
+        <span className="user">
+        <User
+        name={props.tweet.user.name}
+        handle={props.tweet.user.handle} ></User>
+        </span>
+        <span className="timestamp"><Timestamp time={props.tweet.timestamp}></Timestamp></span>
       </div>
-
+      <p className="message">
+        <Message message={props.tweet.message}></Message> 
+      </p>
+      <div className="actions">
+        <Actions></Actions>
+      </div>
+    </div>
       <i className="fas fa-ellipsis-h"></i>
     </div>
   );
