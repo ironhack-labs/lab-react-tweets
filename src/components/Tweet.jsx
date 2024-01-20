23ccec87abae76b55495b111738a477203386963
Vladimir Ilovaca <vladoilovaca@gmail.com>
2024-01-20 11:48:24 +0100

    done

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,34 +1,24 @@
-function Tweet() {
+import User from "./User";
+import Image from "./ProfileImage"
+import TimeStamp from "./Timestamp";
+import Message from "./Message";
+import Actions from "./Actions";
+
+function Tweet({ tweet }) {
   return (
     <div className="tweet">
-      <img
-        src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/ih_logo.jpeg"
-        className="profile"
-        alt="profile"
-      />
+      <Image IMAGE_URL={tweet.user.image}/>
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
+          <User name={tweet.user.name} handle={tweet.user.handle} />
 
-          <span className="timestamp">Nov 30, 2020</span>
+          <TimeStamp TWEET_TIMESTAMP={tweet.timestamp}/>
         </div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
+        <Message TWEET_MESSAGE={tweet.message}/>
 
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment" data-testid="comment-icon"></i>
-          <i className="fas fa-retweet" data-testid="retweet-icon"></i>
-          <i className="far fa-heart" data-testid="heart-icon"></i>
-          <i className="fas fa-share" data-testid="share-icon"></i>
-        </div>
+        <Actions />
       </div>
 
       <i className="fas fa-ellipsis-h"></i>
