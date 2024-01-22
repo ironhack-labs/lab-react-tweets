ff194fdc5780b1151604263237de2b63ef8269aa
Juan Morales Martinez <juanmoralesmartinez@Juans-MacBook-Pro.local>
2024-01-22 22:30:26 +0100

    components

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,36 +1,26 @@
-function Tweet(props) {
-  const {user, timestamp, message} = props.tweet
+import ProfileImage from "./ProfileImage";
+import User from "./User";
+import Timestamp from "./Timestamp";
+import Message from "./Message";
+import Actions from "./Actions";
 
+function Tweet(props) {
 
   return (
     <div className="tweet">
-      <img 
-        src={user.image}
-        className="profile"
-        alt="profile"
-      />
+     
+     <ProfileImage image={props.tweet.user.image}></ProfileImage>
 
       <div className="body">
         <div className="top">
-          <span className="user"> 
-            <span className="name">{user.name}</span>
-            <span className="handle"> {user.handle}</span>
-          </span>
+         <User name={props.tweet.user.name} handle={props.tweet.user.handle} ></User>
 
-          <span className="timestamp">{timestamp}</span>
+          <Timestamp timestamp={props.tweet.timestamp}></Timestamp>
         </div>
 
-        <p className="message">{message}
-        
-        </p>
+        <Message message={props.tweet.message}></Message>
 
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment" data-testid="comment-icon"></i>
-          <i className="fas fa-retweet" data-testid="retweet-icon"></i>
-          <i className="far fa-heart" data-testid="heart-icon"></i>
-          <i className="fas fa-share" data-testid="share-icon"></i>
-        </div>
+       <Actions></Actions>
       </div>
 
       <i className="fas fa-ellipsis-h"></i>
