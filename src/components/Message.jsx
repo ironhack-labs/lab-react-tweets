9bee2832c21504bc00e65ca0395249482818f1a1
Vitor193 <vitormiguel135@gmail.com>
2024-02-06 10:34:43 +0000

    'done'

---
+++ src/components/Message.jsx
@@ -0,0 +1,7 @@
+const Message = ({ TWEET_MESSAGE }) => {
+    return (
+        <p className="message"> {TWEET_MESSAGE} </p>
+    )
+}
+
+export default Message
---
+++ src/components/ProfileImage.jsx
@@ -0,0 +1,7 @@
+const Image = ({ IMAGE_URL }) => {
+    return (
+        <img src={IMAGE_URL} className="profile" alt="profile" />
+    )
+}
+
+export default Image
