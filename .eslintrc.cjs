ba02de91ec55cbb41f9e94426b9558fdcec7596f
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-02-26 22:24:38 -0400

    modified to include prettier and rect/propties off

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,12 +5,13 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
+    'prettier'
   ],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
   plugins: ["react-refresh"],
   rules: {
     "react-refresh/only-export-components": "warn",
-    "react/prop-types": 0,
+    "react/prop-types": 'off'
   },
 };
