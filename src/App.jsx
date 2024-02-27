25311abf942f87570437c006841e1aa7f504fe64
Roxangelica Rolan <rrolanbaez@gmail.com>
2024-02-27 13:58:39 -0400

    Lab done

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,13 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet />
+
+      {/* Iteration 9 */}
+      {
+        tweetsArray.map((tweet) => {
+        return <Tweet key={tweet.message} tweet={tweet} />
+        })
+      }
     </div>
   );
 }
---
+++ src/components/ProfileImage.jsx
@@ -0,0 +1,6 @@
+// Iteration 4
+const ProfileImage = ({ image }) => {
+  return <img src={image} className="profile" alt="profile" />;
+};
+
+export default ProfileImage;
