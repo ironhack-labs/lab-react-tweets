d787b366a318fd17377cfdf161c6caca5afff81f
arebac <j.cabreraortiz@outlook.com>
2024-02-27 13:48:56 -0400

    Lab done!

---
+++ src/components/ProfileImage.jsx
@@ -0,0 +1,15 @@
+
+
+const ProfileImage = ({image}) => {
+  return (
+  
+    <img
+      src={image}
+      className="profile"
+      alt="profile"
+    />
+
+  )
+}
+
+export default ProfileImage
