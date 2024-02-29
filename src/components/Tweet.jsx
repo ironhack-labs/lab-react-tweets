3a66ff9eec530b9fd66ca0c5a1ec15d857f2b851
Arnau Ramos <arnauramos21@gmail.com>
2024-02-29 20:34:33 +0100

    Done iteration 2

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,8 +1,11 @@
-function Tweet() {
+function Tweet(props) {
+  
+  const {user, timestamp, message} = props.tweet
+  const {name, image, handle} = user
   return (
     <div className="tweet">
       <img
-        src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/ih_logo.jpeg"
+        src={image}
         className="profile"
         alt="profile"
       />
@@ -10,16 +13,15 @@ function Tweet() {
       <div className="body">
         <div className="top">
           <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
+            <span className="name">{name}</span>
+            <span className="handle">{handle}</span>
           </span>
 
-          <span className="timestamp">Nov 30, 2020</span>
+          <span className="timestamp">{timestamp}</span>
         </div>
 
         <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
+          {message}
         </p>
 
         <div className="actions">
