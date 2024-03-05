fb74a50a788035ed65f0426652b2dbba21020a92
Ama Williams <53152990+Tetsewa@users.noreply.github.com>
2024-03-05 06:08:36 +0100

    done

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -10,24 +10,26 @@ function Tweet(props) {
   /*const { User, Message, Timestamp } = Tweet*/
   return (
     <div className="tweet">
-    
+
       <ProfileImage profileImage = {props.tweet.user.image}/>
 
-      <div className="body">
-        <div className="top">
-          <User userProp = {props.tweet.user}/>
+      <div>
+        <div className="body">
+          <div className="top">
+            <User userProp = {props.tweet.user}/>
 
-          <Timestamp time = {props.tweet.timestamp}/>
+            <Timestamp time = {props.tweet.timestamp}/>
+          </div>
+
+          <Message message = {props.tweet.message}/>
+
+          
         </div>
 
-        <Message message = {props.tweet.message}/>
-
-        
-      </div>
-
-      <div className="actions">
-        <Actions className="far fa-comment" data-testid="comment-icon" />
-  
+        <div className="actions">
+          <Actions className="far fa-comment" data-testid="comment-icon" />
+    
+        </div>
       </div>
     </div>
   
