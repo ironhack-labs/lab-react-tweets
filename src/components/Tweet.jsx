6c86c217d6a8e5036931e9d026ea1c3cee3d6ee1
Sowjanyakambhampati <93071280+Sowjanyakambhampati@users.noreply.github.com>
2024-03-05 10:01:00 +0100

    Update Tweet.jsx

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -7,30 +7,27 @@ import Actions from "./Actions";
 
 
 function Tweet(props) {
-  /*const { User, Message, Timestamp } = Tweet*/
+ /* const { User, Message, Timestamp } = Tweet*/
   return (
     <div className="tweet">
+      <ProfileImage image = {props.tweet.user.image}/>
 
-      <ProfileImage profileImage = {props.tweet.user.image}/>
+      <div className="body">
+        <div className="top">
+        <User name={props.tweet.user.name} handle={props.tweet.user.handle} />
 
-      <div>
-        <div className="body">
-          <div className="top">
-            <User userProp = {props.tweet.user}/>
-
-            <Timestamp time = {props.tweet.timestamp}/>
-          </div>
-
-          <Message message = {props.tweet.message}/>
-
-          
+          <Timestamp time = {props.tweet.timestamp}/>
         </div>
 
-        <div className="actions">
-          <Actions className="far fa-comment" data-testid="comment-icon" />
-    
-        </div>
+        <Message message = {props.tweet.message}/>
+
+        
       </div>
+
+      <div className="Actions">
+  <Actions className="far fa-comment" data-testid="comment-icon" />
+  
+</div>
     </div>
   
   );
