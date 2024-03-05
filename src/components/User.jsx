cbdfa7fa4e231cf5af81f333731d3fbaa13ae616
Sowjanyakambhampati <93071280+Sowjanyakambhampati@users.noreply.github.com>
2024-03-05 10:00:45 +0100

    Update User.jsx

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,9 +1,8 @@
 function User(props) {
-    // console.log(props)
   return (
     <span className="user">
-      <span className="name"> {props.userProp.name} </span>
-      <span className="handle">{props.userProp.handle}</span>
+      <span className="name">{props.name}</span>
+      <span className="handle">@{props.handle}</span>
     </span>
   );
 }
